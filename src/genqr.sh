9a92327898baf9212217e3b51a4a5c5acf9020bd
Amir Taaki <genjix@riseup.net>
2013-07-22 14:42:18 +0200

    added dest file to qr code generator.

--- src/genqr.sh
+++ src/genqr.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-    echo "Usage: genqr ADDRESS"
+if [ $# -ne 2 ]; then
+    echo "Usage: genqr ADDRESS FILENAME"
     exit 1
 fi
 command -v qrencode >/dev/null 2>&1 || { echo >&2 "Please install 'qrencode'."; exit 1; }
 ADDR=$1
-qrencode bitcoin:$ADDR -o qrcode.png -s 10
+FILE=$2
+qrencode bitcoin:$ADDR -o $FILE -s 10
 
