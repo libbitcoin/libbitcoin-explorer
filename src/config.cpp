24b549d0419c42e78fdbb4ef88711c0416e87231
Amir Taaki <genjix@riseup.net>
2013-07-24 06:45:28 +0200

    sx config file is now ~/.sx.cfg

--- src/config.cpp
+++ src/config.cpp
@@ -1,8 +1,25 @@
 #include "config.hpp"
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 #include <boost/filesystem.hpp>
 #include <boost/lexical_cast.hpp>
 #include <libconfig.h++>
+
+using boost::filesystem::path;
+
+path home_directory()
+{
+    const char* home_path = getenv("HOME");
+    if (!home_path)
+    {
+        passwd* pw = getpwuid(getuid());
+        const char *homedir = pw->pw_dir;
+        return path(homedir);
+    }
+    return path(home_path);
+}
 
 template <typename T>
 void get_value(const libconfig::Setting& root, config_map_type& config,
@@ -17,8 +34,7 @@ void get_value(const libconfig::Setting& root, config_map_type& config,
 
 void load_config(config_map_type& config)
 {
-    using boost::filesystem::path;
-    path conf_path = path(SYSCONFDIR) / "sx.cfg";
+    path conf_path = home_directory() / ".sx.cfg";
     libconfig::Config cfg;
     // Ignore error if unable to read config file.
     try
@@ -29,6 +45,6 @@ void load_config(config_map_type& config)
     catch (const libconfig::ParseException&) {}
     // Read off values
     const libconfig::Setting& root = cfg.getRoot();
-    get_value<std::string>(root, config, "service", "tcp://localhost:9091");
+    get_value<std::string>(root, config, "service", "tcp://46.4.92.107:9091");
 }
 
