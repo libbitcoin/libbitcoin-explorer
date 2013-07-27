d91a1d7b65fc830f22e624c55eeb481fe953af50
Amir Taaki <genjix@riseup.net>
2013-07-27 02:27:45 +0200

    history with nicer output.

--- src/history.cpp
+++ src/history.cpp
@@ -4,11 +4,15 @@
 #include "util.hpp"
 
 using namespace bc;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+typedef std::vector<payment_address> payaddr_list;
 
 bool stopped = false;
 
-void history_fetched(const std::error_code& ec,
-    const blockchain::history_list& history)
+void history_fetched(const payment_address& payaddr,
+    const std::error_code& ec, const blockchain::history_list& history)
 {
     if (ec)
     {
@@ -16,35 +20,68 @@ void history_fetched(const std::error_code& ec,
             << ec.message() << std::endl;
         return;
     }
-    std::cout << "Output Hash:Index\t\t\t\t\t\t\tOutput Height\t"
-        << "Value (Satoshis)\t\t\tSpend Hash:Index\t\t\t\t"
-        << "Spend Height" << std::endl;
     for (const auto& row: history)
     {
-        std::cout << row.output.hash << ":" << row.output.index << "\t";
-        std::cout << row.output_height << "\t\t";
-        std::cout << row.value << "\t\t";
+        std::cout << "Address: " << payaddr.encoded() << std::endl;
+        std::cout << "  output: " << row.output
+            << "  height: " << row.output_height << std::endl;
+        std::cout << "  value:  " << row.value << std::endl;
+        std::cout << "  spend:  ";
         if (row.spend.hash == null_hash)
-            std::cout << "Unspent\t\t\t\t\t\t\t\t\t";
+        {
+            std::cout << "Unspent";
+        }
         else
-            std::cout << row.spend.hash << ":" << row.spend.index << "\t";
-        std::cout << row.spend_height << std::endl;
+        {
+            std::cout << row.spend << "  height: " << row.spend_height;
+        }
+        std::cout << std::endl << std::endl;
     }
     stopped = true;
 }
 
-int main()
+bool payaddr_from_stdin(payment_address& payaddr)
 {
-    config_map_type config;
-    load_config(config);
-    payment_address payaddr;
     if (!payaddr.set_encoded(read_stdin()))
     {
         std::cerr << "history: Invalid address." << std::endl;
-        return -1;
+        return false;
+    }
+    return true;
+}
+
+bool payaddr_from_argv(payaddr_list& payaddrs, int argc, char** argv)
+{
+    for (size_t i = 1; i < argc; ++i)
+    {
+        payment_address payaddr;
+        if (!payaddr.set_encoded(argv[i]))
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    config_map_type config;
+    load_config(config);
+    payaddr_list payaddrs;
+    if (argc == 1)
+    {
+        payment_address payaddr;
+        if (!payaddr_from_stdin(payaddr))
+            return -1;
+        payaddrs.push_back(payaddr);
+    }
+    else
+    {
+        if (!payaddr_from_argv(payaddrs, argc, argv))
+            return -1;
     }
     fullnode_interface fullnode(config["service"]);
-    fullnode.blockchain.fetch_history(payaddr, history_fetched);
+    for (const payment_address& payaddr: payaddrs)
+        fullnode.blockchain.fetch_history(payaddr,
+            std::bind(history_fetched, payaddr, _1, _2));
     while (!stopped)
     {
         fullnode.update();
