35f3a7b3fd625c5df3a18cc6ff0c548cec818a93
Amir Taaki <genjix@riseup.net>
2013-08-09 17:19:35 +0200

    forgot file.

---
+++ src/fetch-last-height.cpp
@@ -0,0 +1,35 @@
+#include <bitcoin/bitcoin.hpp>
+#include <obelisk/client/interface.hpp>
+#include "config.hpp"
+#include "util.hpp"
+
+using namespace bc;
+
+bool stopped = false;
+
+void height_fetched(const std::error_code& ec, size_t height)
+{
+    if (ec)
+    {
+        std::cerr << "fetch-last-height: " << ec.message() << std::endl;
+        stopped = true;
+        return;
+    }
+    std::cout << height << std::endl;
+    stopped = true;
+}
+
+int main()
+{
+    config_map_type config;
+    load_config(config);
+    fullnode_interface fullnode(config["service"]);
+    fullnode.blockchain.fetch_last_height(height_fetched);
+    while (!stopped)
+    {
+        fullnode.update();
+        sleep(0.1);
+    }
+    return 0;
+}
+
