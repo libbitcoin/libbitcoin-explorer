0b6cad2e2f26aa86d95bc388c4ce053ce8c0f4cb
Amir Taaki <genjix@riseup.net>
2013-09-03 11:05:14 +0200

    -j for history to convert to json.

--- src/history.cpp
+++ src/history.cpp
@@ -9,7 +9,8 @@ using std::placeholders::_2;
 
 typedef std::vector<payment_address> payaddr_list;
 
-std::atomic_uint fetch_count(0);
+std::atomic_uint remaining_count;
+bool json_output = false;
 
 void history_fetched(const payment_address& payaddr,
     const std::error_code& ec, const blockchain::history_list& history)
@@ -48,7 +49,58 @@ void history_fetched(const payment_address& payaddr,
         }
         std::cout << std::endl;
     }
-    ++fetch_count;
+    --remaining_count;
+}
+
+void json_history_fetched(const payment_address& payaddr,
+    const std::error_code& ec, const blockchain::history_list& history)
+{
+    if (ec)
+    {
+        std::cerr << "history: Failed to fetch history: "
+            << ec.message() << std::endl;
+        return;
+    }
+    bool is_first = true;
+    for (const auto& row: history)
+    {
+        // Put commas between each array item in json output.
+        if (is_first)
+            is_first = false;
+        else
+            std::cout << "," << std::endl;
+        // Actual row data.
+        std::cout << "{" << std::endl;
+        std::cout << "  \"address\": \"" << payaddr.encoded()
+            << "\"," << std::endl;
+        std::cout << "  \"output\": \"" << row.output
+            << "\"," << std::endl;
+        std::cout << "  \"output_height\": ";
+        if (!row.output_height)
+            std::cout << "\"Pending\"";
+        else
+            std::cout << row.output_height;
+        std::cout << "," << std::endl;
+        std::cout << "  \"value\":  \"" << row.value << "\"," << std::endl;
+        if (row.spend.hash == null_hash)
+        {
+            std::cout << "  \"spend\": \"Unspent\"," << std::endl;
+            std::cout << "  \"spend_height\": \"Unspent\"" << std::endl;
+        }
+        else
+        {
+            std::cout << "  \"spend\": \"" << row.spend << "\"," << std::endl;
+            std::cout << "  \"spend_height\": ";
+            if (!row.spend_height)
+                std::cout << "\"Pending\"";
+            else
+                std::cout << "\"" << row.spend_height << "\"";
+        }
+        std::cout << "}";
+    }
+    if (--remaining_count > 0)
+        std::cout << ",";
+    std::cout << std::endl;
 }
 
 bool payaddr_from_stdin(payment_address& payaddr)
@@ -65,8 +117,14 @@ bool payaddr_from_argv(payaddr_list& payaddrs, int argc, char** argv)
 {
     for (size_t i = 1; i < argc; ++i)
     {
+        const std::string arg = argv[i];
+        if (arg == "-j")
+        {
+            json_output = true;
+            continue;
+        }
         payment_address payaddr;
-        if (!payaddr.set_encoded(argv[i]))
+        if (!payaddr.set_encoded(arg))
             return false;
         payaddrs.push_back(payaddr);
     }
@@ -90,17 +148,27 @@ int main(int argc, char** argv)
         if (!payaddr_from_argv(payaddrs, argc, argv))
             return -1;
     }
+    remaining_count = payaddrs.size();
     threadpool pool(1);
     obelisk::fullnode_interface fullnode(pool, config["service"]);
+    if (json_output)
+        std::cout << "[" << std::endl;
     for (const payment_address& payaddr: payaddrs)
-        fullnode.address.fetch_history(payaddr,
-            std::bind(history_fetched, payaddr, _1, _2));
-    while (fetch_count < payaddrs.size())
+    {
+        if (json_output)
+            fullnode.address.fetch_history(payaddr,
+                std::bind(json_history_fetched, payaddr, _1, _2));
+        else
+            fullnode.address.fetch_history(payaddr,
+                std::bind(history_fetched, payaddr, _1, _2));
+    }
+    while (remaining_count > 0)
     {
         fullnode.update();
         sleep(0.1);
     }
-    BITCOIN_ASSERT(fetch_count == payaddrs.size());
+    if (json_output)
+        std::cout << "]" << std::endl;
     pool.stop();
     pool.join();
     return 0;
