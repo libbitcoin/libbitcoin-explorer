ac858380a63ce90b36bd2c4bcbecd59c7c9044b9
Amir Taaki <genjix@riseup.net>
2013-10-03 18:22:21 +0200

    stop limit for genpub so people can generate a range of pubkeys.

--- src/genpub.cpp
+++ src/genpub.cpp
@@ -12,9 +12,9 @@ std::string read_data()
 
 int main(int argc, char** argv)
 {
-    if (argc != 2 && argc != 3)
+    if (argc < 2 || argc > 4)
     {
-        std::cerr << "Usage: genaddr N [CHANGE]" << std::endl;
+        std::cerr << "Usage: genaddr N [CHANGE] [RANGESTOP]" << std::endl;
         return -1;
     }
     size_t n;
@@ -28,12 +28,30 @@ int main(int argc, char** argv)
         return -1;
     }
     bool for_change = false;
-    if (argc == 3)
+    if (argc >= 3)
     {
         std::string change_str = argv[2];
         boost::algorithm::to_lower(change_str);
         if (change_str == "true" || change_str == "1")
             for_change = true;
+    }
+    size_t stop_limit = n;
+    if (argc == 4)
+    {
+        try
+        {
+            stop_limit = boost::lexical_cast<size_t>(argv[3]);
+        }
+        catch (const boost::bad_lexical_cast&)
+        {
+            std::cerr << "genaddr: Bad RANGESTOP provided" << std::endl;
+            return -1;
+        }
+    }
+    if (stop_limit < n)
+    {
+        std::cerr << "genaddr: RANGESTOP cannot be less than N" << std::endl;
+        return -1;
     }
     deterministic_wallet wallet;
     std::string user_data = read_data();
@@ -47,8 +65,11 @@ int main(int argc, char** argv)
             return -1;
         }
     }
-    data_chunk pubkey = wallet.generate_public_key(n, for_change);
-    std::cout << pubkey << std::endl;
+    for (size_t i = n; i <= stop_limit; ++i)
+    {
+        data_chunk pubkey = wallet.generate_public_key(i, for_change);
+        std::cout << pubkey << std::endl;
+    }
     return 0;
 }
 
