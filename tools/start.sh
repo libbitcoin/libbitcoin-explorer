5a31d4ff80fdc516a17a805aa59d7b617d747fe5
Dr Washington Sanchez <washington.sanchez@gmail.com>
2014-04-03 02:57:01 +1000

    Update start.sh
    1. Creates time-stamped text files for the public key as well as the private key
    2. Changed the name of the function to avoid confusion
    3. Changed the goodbye message

--- tools/start.sh
+++ tools/start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-function addr {
+function keypair {
 	sx newkey > privatekey-$(date +%y-%m-%d-%s).txt
+	cat privatekey-$(date +%y-%m-%d-%s).txt | sx addr > publickey-$(date +%y-%m-%d-%s).txt
 }
+
 function stealth {
 	sx stealth-new > stealthkeys-$(date +%y-%m-%d-%s).txt
 }
 
-if zenity --question --text="Do you want to create a new private key?"; then
-	addr
+if zenity --question --text="Do you want to create a new keypair?"; then
+	keypair
 	if zenity --question --text="Do you want to generate a stealth address?"; then
 		stealth
 	else
@@ -19,5 +21,5 @@ elif zenity --question --text="Do you want to generate a stealth address?"; then
 	stealth
 	zenity --info --text="Done. Enjoy your privacy\!"
 else
-	zenity --info --text="Thanks for stopping by\!"
+	zenity --info --text="Thanks for using SX\!"
 fi
