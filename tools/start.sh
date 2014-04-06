92d7dc4818b6afef2884aea85eface04db9b2a31
Dr Washington Sanchez <washington.sanchez@gmail.com>
2014-04-06 16:40:56 +1000

    Update start.sh
    Added the ability to:
    
    1. Stealth: generate an ephemeral key and Bitcoin public key for the recipient
    2. Stealth: check whether an ephemeral key from a stealth transaction matches your private stealth key, if so the resulting text file will give you a Bitcoin keypair (presumably with funds 'attached' to them)

--- tools/start.sh
+++ tools/start.sh
@@ -13,21 +13,46 @@ function stealth {
 	sx stealth-new > stealthkeys-$(date +%y-%m-%d-%s).txt
 }
 
+#Obtain stealth transaction details
+
+function sendstealth {
+	sx stealth-send $(zenity --entry --title="Stealth Transaction Details" --text="Enter the stealth public address of the recipient.") > stealthrecipientaddr-$(date +%y-%m-%d-%s).txt	
+}
+
+#Check stealth transaction details
+
+function receivestealth {
+	yourstealthpriv=$(zenity --entry --title="Stealth Transaction Details" --text="Enter the stealth transaction ephemeral key.")
+	ephem=$(zenity --entry --title="Stealth Transaction Details" --text="Enter your stealth private key.")
+	sx stealth-recv $yourstealthpriv $ephem > stealthbitcoins-$(date +%y-%m-%d-%s).txt
+	zenity --info --height=150 --text="Open the stealthbitcoins.txt file to obtain the Bitcoin key pairs from the stealth transaction. If your ephemeral and/or stealth keys were incorrect, this text file will be blank."
+}
+
 #Create a variable that is the output
 # of the user's selection in the list
 # dialog box.
 
-OUT=$(zenity --list --checklist --text="Select an action from the list below" --title="SX Tools Menu" --column="Select" --column="Create" TRUE "New Address" FALSE "Stealth Address")
+Menu=$(zenity --list --radiolist --width=800 --height=250 \
+--text="Select an action from the list below" --title="SX Tools Menu" \
+--column="Select" --column="Action" --column="Description" \
+FALSE "New Address" "Generate a new Bitcoin address" \
+FALSE "Stealth Address" "Generate a new stealth key pair: address (public) and secret (private)." \
+FALSE "Send Stealth Transaction" "Generate a stealth ephemeral key and Bitcoin public key to send a stealth transaction." \
+FALSE "Receive Stealth Transaction" "Attempt to generate a Bitcoin keypair from a stealth ephemeral key")
 
 #Pseudo-menu based on Zenity's list dialog box
 
-if [ "$OUT" == "New Address" ]; then
+if [ "$Menu" == "New Address" ]; then
 	keypair
-elif [ "$OUT" == "Stealth Address" ]; then
+	zenity --info --text="Open the publickey.txt and privatekey.txt files to obtain Bitcoin keys."
+elif [ "$Menu" == "Stealth Address" ]; then
 	stealth
-elif [ "$OUT" == "New Address|Stealth Address" ]; then
-	keypair
-	stealth
+	zenity --info --text="Open the stealthkeys.txt file to obtain your stealth key pairs."
+elif [ "$Menu" == "Send Stealth Transaction" ]; then
+	sendstealth
+	zenity --info --text="Open the stealthrecipient.txt file to obtain the ephemeral (shared secret) and the recipient's Bitcoin public key."
+elif [ "$Menu" == "Receive Stealth Transaction" ]; then
+	receivestealth
 else
 	zenity --info --text="Thanks for using SX\!"
 fi
