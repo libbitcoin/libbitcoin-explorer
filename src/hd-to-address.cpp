cce3a93fac5c4a061cb2669883963c2f568e5fd6
Amir Taaki <genjix@riseup.net>
2014-04-24 07:46:03 +0000

    hd-to-address and hd-to-wif commands.

--- src/hd-to-address.cpp
+++ src/hd-to-address.cpp
@@ -7,6 +7,19 @@ using namespace libwallet;
 int main()
 {
     // Read key from STDIN.
+    std::string encoded_key = read_stdin();
+    hd_public_key key;
+
+    // First try loading private key.
+    hd_private_key private_key;
+    if (private_key.set_serialized(encoded_key))
+        key = private_key;
+    else if (!key.set_serialized(encoded_key))
+    {
+        std::cerr << "hd-pub: error reading key." << std::endl;
+        return -1;
+    }
+    std::cout << key.address().encoded() << std::endl;
     return 0;
 }
 
