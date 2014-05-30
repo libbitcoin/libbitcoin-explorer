e7df97eeed7400a5c8dd12578ee5a70b076cf2dd
evoskuil <eric@voskuil.org>
2014-05-30 11:41:19 -0700

    Move main.cpp to console/

--- src/console/main.cpp
+++ src/console/main.cpp
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include "command_line.hpp"
+#include <sx/sx.hpp>
 
 /**
  * Invoke the sx library with the raw arguments provided on the command line.
