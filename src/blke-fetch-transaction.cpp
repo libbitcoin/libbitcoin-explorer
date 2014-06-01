a0f772dea50655e0e0c1f19e801e3f049c67848f
evoskuil <eric@voskuil.org>
2014-06-01 13:32:24 -0700

    Stub in remaining classes for python command port.

--- src/blke-fetch-transaction.cpp
+++ src/blke-fetch-transaction.cpp
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2011-2014 sx developers (see AUTHORS)
+ *
+ * This file is part of sx.
+ *
+ * sx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <sx/command/blke-fetch-transaction.hpp>
+#include <sx/command/unwrap.hpp>
+#include <sx/utility/console.hpp>
+#include <sx/utility/command_line.hpp>
+
+bool sx::extensions::blke_fetch_transaction::invoke(const int argc, const char* argv[])
+{
+    line_out(std::cerr, "sx: This command is not yet ported from python.");
+    return false;
+}
+
 //#!/usr/bin/python
 //import urllib, urllib2, sys, json, copy, random, subprocess
 //PIPE = subprocess.PIPE
