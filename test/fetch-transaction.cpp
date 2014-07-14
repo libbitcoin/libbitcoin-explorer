2297d31b491b6eb807cd561413ff43c43030a804
evoskuil <eric@voskuil.org>
2014-07-14 16:38:50 -0700

    Add fetch tests.

---
+++ test/fetch-transaction.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2011-2014 sx developers (see AUTHORS)
+ *
+ * This file is part of sx.
+ *
+ * sx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <iostream>
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test_suite.hpp>
+#include <sx/sx.hpp>
+#include "command.hpp"
+
+SX_USING_NAMESPACES()
+
+// This is a namespace for tests by class/file__method/function.
+BOOST_AUTO_TEST_SUITE(fetch_transaction__invoke)
+
+//BOOST_AUTO_TEST_CASE(fetch_transaction__invoke__always__okay)
+//{
+//    // $ sx fetch-transaction 69735d70ada1be32ff39b49c6fc2390b03e9d5eed8918ed10fe42c8cbabf62d4
+//    SX_DECLARE_COMMAND(fetch_transaction);
+//    SX_REQUIRE_OKAY(command.invoke(input, output, error));
+//}
+
+BOOST_AUTO_TEST_SUITE_END()
