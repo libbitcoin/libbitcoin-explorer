430a8c8a7f7188278a1b31dba7f45d321559ebea
evoskuil <eric@voskuil.org>
2014-07-29 02:19:55 -0700

    order includes

--- src/sign-input.cpp
+++ src/sign-input.cpp
@@ -17,9 +17,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#include <sx/command/sign-input.hpp>
+
 #include <iostream>
 #include <bitcoin/bitcoin.hpp>
-#include <sx/command/sign-input.hpp>
 #include <sx/utility/utility.hpp>
 
 using namespace bc;
