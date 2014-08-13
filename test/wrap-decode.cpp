7cb4f68495ca17d620c3854120d64c7b3488d10c
evoskuil <eric@voskuil.org>
2014-08-13 03:33:29 -0700

    Update tests, now all 310 passing.

--- test/wrap-decode.cpp
+++ test/wrap-decode.cpp
@@ -24,6 +24,26 @@ SX_USING_NAMESPACES()
 
 BOOST_AUTO_TEST_SUITE(wrap_decode__invoke)
 
+BOOST_AUTO_TEST_CASE(wrap_decode__invoke__valid_checksum_native__okay_output)
+{
+    // $ sx wrap-decode 2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4 -f native
+    SX_DECLARE_COMMAND(wrap_decode);
+    command.set_format_option({ encoding_engine::native });
+    command.set_wrapped_argument({ "2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4" });
+    SX_REQUIRE_OKAY(command.invoke(output, error));
+    SX_REQUIRE_OUTPUT("wrapper\n{\n    version 42\n    payload 031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\n    checksum 3840642601\n}\n\n");
+}
+
+BOOST_AUTO_TEST_CASE(wrap_decode__invoke__valid_checksum_info__okay_output)
+{
+    // $ sx wrap-decode 2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4 -f info
+    SX_DECLARE_COMMAND(wrap_decode);
+    command.set_format_option({ encoding_engine::info });
+    command.set_wrapped_argument({ "2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4" });
+    SX_REQUIRE_OKAY(command.invoke(output, error));
+    SX_REQUIRE_OUTPUT("wrapper\n{\n    version 42\n    payload 031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\n    checksum 3840642601\n}\n\n");
+}
+
 BOOST_AUTO_TEST_CASE(wrap_decode__invoke__valid_checksum_json__okay_output)
 {
     // $ sx wrap-decode 2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4 -f json
@@ -31,8 +51,17 @@ BOOST_AUTO_TEST_CASE(wrap_decode__invoke__valid_checksum_json__okay_output)
     command.set_format_option({ encoding_engine::json });
     command.set_wrapped_argument({ "2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4" });
     SX_REQUIRE_OKAY(command.invoke(output, error));
-    SX_REQUIRE_OUTPUT("wrapper\n{ \n    version 42\n    payload 031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\n    checksum 3840642601\n }\n");
+    SX_REQUIRE_OUTPUT("{\n    \"wrapper\": {\n        \"version\": \"42\",\n        \"payload\": \"031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\",\n        \"checksum\": \"3840642601\"\n    }\n}\n\n");
+}
+
+BOOST_AUTO_TEST_CASE(wrap_decode__invoke__valid_checksum_xml__okay_output)
+{
+    // $ sx wrap-decode 2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4 -f xml
+    SX_DECLARE_COMMAND(wrap_decode);
+    command.set_format_option({ encoding_engine::xml });
+    command.set_wrapped_argument({ "2a031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006298eebe4" });
+    SX_REQUIRE_OKAY(command.invoke(output, error));
+    SX_REQUIRE_OUTPUT("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<wrapper><version>42</version><payload>031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006</payload><checksum>3840642601</checksum></wrapper>\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-
