9c3efb8ba123e3197af31293391a5029ba7b411c
evoskuil <eric@voskuil.org>
2014-08-28 00:40:42 -0700

    Define boost test symbol before PCH.

--- test/main.cpp
+++ test/main.cpp
@@ -17,6 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include "precompile.hpp"
 #define BOOST_TEST_MODULE libbitcoin_explorer_test
+#include "precompile.hpp"
 #include <boost/test/unit_test.hpp>
