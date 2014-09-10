e0a4dbca57efb00a3a471a5394f9440eaceeab6e
evoskuil <eric@voskuil.org>
2014-09-10 15:09:15 -0700

    Fix test include paths.

--- test/printer.cpp
+++ test/printer.cpp
@@ -23,7 +23,7 @@
 #include <boost/program_options.hpp>
 #include <boost/test/test_tools.hpp>
 #include <boost/test/unit_test_suite.hpp>
-#include <explorer/explorer.hpp>
+#include <bitcoin/explorer.hpp>
 
 BOOST_AUTO_TEST_SUITE(printer__initialize)
 
