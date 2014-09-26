486eac68d482d3d764e115ecf51a8fc45b0ec0f9
evoskuil <eric@voskuil.org>
2014-09-26 14:38:30 -0700

    Add signle input test for input-sign command.

--- test/commands/input-sign.cpp
+++ test/commands/input-sign.cpp
@@ -24,10 +24,31 @@ BX_USING_NAMESPACES()
 
 BOOST_AUTO_TEST_SUITE(input_sign__invoke)
 
-BOOST_AUTO_TEST_CASE(input_sign__invoke__always__failure_error)
+#define INPUT_SIGN_WIF_A "5KPFsatiYrJcvCSRrDbtx61822vZjeGGGx3wu38pQDHRF8eVJ8H"
+#define INPUT_SIGN_NONCE_A "000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f"
+#define INPUT_SIGN_TX_A "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970000000000ffffffff01905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
+#define INPUT_SIGN_SIGNATURE_A "3045022100d303bbe206ef6bc5c1033044a36145aac642600dbe9e0f1b725cd9f16c719bfe022045c57877f5242e0311c705713cffec6eb372949970b066988756bca30b7c6755"
+
+
+BOOST_AUTO_TEST_CASE(input_sign__invoke__single_input__okay_output)
 {
-    //BX_DECLARE_COMMAND(input_sign);
-    //BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_DECLARE_COMMAND(input_sign);
+
+    command.set_index_option(0);
+    command.set_sighash_option({ "single" });
+    command.set_nonce_argument({ INPUT_SIGN_NONCE_A });
+    command.set_transaction_argument({ INPUT_SIGN_TX_A });
+
+    auto secret = wif_to_secret(INPUT_SIGN_WIF_A);
+    command.set_ec_private_key_argument(secret);
+
+    std::stringstream prevout;
+    auto hash = bitcoin_short_hash(secret_to_public_key(secret));
+    prevout << "dup hash160 [ " << hash << " ] equalverify checksig";
+    command.set_prevout_script_argument(prevout.str());
+
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(INPUT_SIGN_SIGNATURE_A "\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
