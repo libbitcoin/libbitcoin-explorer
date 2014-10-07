5c5f35599c0055f85ec6eda1d2d5954cbc3d3187
evoskuil <eric@voskuil.org>
2014-10-06 23:34:51 -0700

    Comments

--- src/commands/send-tx-p2p.cpp
+++ src/commands/send-tx-p2p.cpp
@@ -126,7 +126,7 @@ console_result send_tx_p2p::invoke(std::ostream& output, std::ostream& error)
     };
 
     // Set up connections.
-    // TODO: how is mainnet / testnet selected ?
+    // BUGBUG: mainnet/testnet determined by libbitcoin compilation.
     async_client client(*this, 4);
 
     // Create dependencies for our protocol object.
