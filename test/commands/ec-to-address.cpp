22cee319b928d8a745373eefc136b03336a620f9
evoskuil <eric@voskuil.org>
2014-10-14 01:40:15 -0700

    More tests.

--- test/commands/ec-to-address.cpp
+++ test/commands/ec-to-address.cpp
@@ -33,12 +33,30 @@ BOOST_AUTO_TEST_CASE(ec_to_address__invoke__compressed_key__okay_output)
     BX_REQUIRE_OUTPUT("1EKJFK8kBmasFRYY3Ay9QjpJLm4vemJtC1\n");
 }
 
+BOOST_AUTO_TEST_CASE(ec_to_address__invoke__compressed_key_version_42__okay_output)
+{
+    BX_DECLARE_COMMAND(ec_to_address);
+    command.set_version_option(42);
+    command.set_ec_public_key_argument({ "0247140d2811498679fe9a0467a75ac7aa581476c102d27377bc0232635af8ad36" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("J8Wdbser1M3fbdQB4nxXn1FLmwtYXKovfA\n");
+}
+
 BOOST_AUTO_TEST_CASE(ec_to_address__invoke__uncompressed_key__okay_output)
 {
     BX_DECLARE_COMMAND(ec_to_address);
     command.set_ec_public_key_argument({ "0447140d2811498679fe9a0467a75ac7aa581476c102d27377bc0232635af8ad36e87bb04f401be3b770a0f3e2267a6c3b14a3074f6b5ce4419f1fcdc1ca4b1cb6" });
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("197FLrycah42jKDgfmTaok7b8kNHA7R2ih\n");
+}
+
+BOOST_AUTO_TEST_CASE(ec_to_address__invoke__uncompressed_key_version_42__okay_output)
+{
+    BX_DECLARE_COMMAND(ec_to_address);
+    command.set_version_option(42);
+    command.set_ec_public_key_argument({ "0447140d2811498679fe9a0467a75ac7aa581476c102d27377bc0232635af8ad36e87bb04f401be3b770a0f3e2267a6c3b14a3074f6b5ce4419f1fcdc1ca4b1cb6" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("J3JahRViQGWq5X5KhPSyB1YdZwBu2EQzFR\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
