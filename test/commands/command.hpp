97c89bc8b6abfd01b05f9a29c11fd79fc7569c64
evoskuil <eric@voskuil.org>
2014-11-04 13:56:12 -0800

    Change default obelisk server url.

--- test/commands/command.hpp
+++ test/commands/command.hpp
@@ -67,22 +67,23 @@ using namespace bc::explorer::primitives;
 //#define BX_MAINNET_SERVER "tcp://obelisk.unsystem.net:8081"       // timeout
 
 // Production
+#define BX_MAINNET_SERVER "tcp://obelisk.airbitz.co:9091"           // alive
 //#define BX_MAINNET_SERVER "tcp://obelisk.coinkite.com:9091"       // alive
 //#define BX_MAINNET_SERVER "tcp://obelisk.bysh.me:9091"            // timeout
 //#define BX_MAINNET_SERVER "tcp://obelisk.ottrbutt.com:9091"       // timeout
 //#define BX_MAINNET_SERVER "tcp://obelisk-baltic.airbitz.co:9091"  // alive
 //#define BX_MAINNET_SERVER "tcp://obelisk-crate.airbitz.co:9091"   // alive
-#define BX_MAINNET_SERVER "tcp://obelisk-sol.airbitz.co:9091"       // alive
+//#define BX_MAINNET_SERVER "tcp://obelisk-sol.airbitz.co:9091"     // alive
 //#define BX_MAINNET_SERVER "tcp://obelisk-virpus.airbitz.co:9091"  // alive
 
 // Testing
 //#define BX_MAINNET_SERVER "tcp://37.139.11.99:9091"               // timeout
 
 // Testnet
+#define BX_TESTNET_SERVER "tcp://obelisk-testnet.airbitz.co:9091"   // alive
 //#define BX_TESTNET_SERVER "tcp://obelisk.unsystem.net:10091"      // alive
 //#define BX_TESTNET_SERVER "tcp://178.79.185.162:9091"             // timeout
 //#define BX_TESTNET_SERVER "tcp://85.25.198.97:10091"              // alive
-#define BX_TESTNET_SERVER "tcp://obelisk-testnet.airbitz.co:9091"   // alive
 //#define BX_TESTNET_SERVER "tcp://preacher.veox.pw:9091"           // alive
 
 #define BX_DECLARE_COMMAND(extension) \
