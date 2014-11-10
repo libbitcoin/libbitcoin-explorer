b2a1e073ecf6adce7f2fe50a89cce8124200a26e
evoskuil <eric@voskuil.org>
2014-11-10 14:44:53 -0800

    Add corrected BIP-21 vectors to tests, adjust uri tests for recent changes.

--- test/commands/uri-decode.cpp
+++ test/commands/uri-decode.cpp
@@ -25,6 +25,92 @@ BX_USING_NAMESPACES()
 BOOST_AUTO_TEST_SUITE(offline)
 BOOST_AUTO_TEST_SUITE(uri_decode__invoke)
 
+// BIP-21 vectors
+
+#define URI_DECODE_BIP21_ADDRESS \
+"uri\n" \
+"{\n" \
+"    address 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L\n" \
+"    scheme bitcoin\n" \
+"}\n"
+
+#define URI_DECODE_BIP21_ADDRESS_LABEL \
+"uri\n" \
+"{\n" \
+"    address 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L\n" \
+"    label Luke-Jr\n" \
+"    scheme bitcoin\n" \
+"}\n"
+
+#define URI_DECODE_BIP21_ADDRESS_AMOUNT_LABEL \
+"uri\n" \
+"{\n" \
+"    address 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L\n" \
+"    amount 2030000000\n" \
+"    label Luke-Jr\n" \
+"    scheme bitcoin\n" \
+"}\n"
+
+#define URI_DECODE_BIP21_ADDRESS_AMOUNT_LABEL_MESSAGE \
+"uri\n" \
+"{\n" \
+"    address 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L\n" \
+"    amount 5000000000\n" \
+"    label Luke-Jr\n" \
+"    message \"Donation for project xyz\"\n" \
+"    scheme bitcoin\n" \
+"}\n"
+
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__bip21_address__okay_output)
+{
+    BX_DECLARE_COMMAND(uri_decode);
+    command.set_uri_argument({ "bitcoin:1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(URI_DECODE_BIP21_ADDRESS);
+}
+
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__bip21_address_label__okay_output)
+{
+    BX_DECLARE_COMMAND(uri_decode);
+    command.set_uri_argument({ "bitcoin:1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L?label=Luke-Jr" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(URI_DECODE_BIP21_ADDRESS_LABEL);
+}
+
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__bip21_address_amount_label__okay_output)
+{
+    BX_DECLARE_COMMAND(uri_decode);
+    command.set_uri_argument({ "bitcoin:1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L?amount=20.3&label=Luke-Jr" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(URI_DECODE_BIP21_ADDRESS_AMOUNT_LABEL);
+}
+
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__bip21_address_amount_label_message__okay_output)
+{
+    BX_DECLARE_COMMAND(uri_decode);
+    command.set_uri_argument({ "bitcoin:1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L?amount=50&label=Luke-Jr&message=Donation%20for%20project%20xyz" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(URI_DECODE_BIP21_ADDRESS_AMOUNT_LABEL_MESSAGE);
+}
+
+// The required additional options are not modeled.
+//BOOST_AUTO_TEST_CASE(uri_decode__invoke__bip21_address_extra_invalid__throws_invalid_option_value)
+//{
+//    BX_DECLARE_COMMAND(uri_decode);
+//    BX_REQUIRE_THROW_INVALID_OPTION_VALUE(command.set_uri_argument({ "bitcoin:1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L?req-somethingyoudontunderstand=50&req-somethingelseyoudontget=999" }));
+//}
+
+// The allowed additional options are not modeled.
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__bip21_address_extra_valid__okay_output)
+{
+    BX_DECLARE_COMMAND(uri_decode);
+    command.set_uri_argument({ "bitcoin:1NS17iag9jJgTHD1VXjvLCEnZuQ3rJED9L?somethingyoudontunderstand=50&somethingelseyoudontget=999" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(URI_DECODE_BIP21_ADDRESS);
+}
+
+// additional (similar to libbitcoin vectors)
+
 #define URI_DECODE_EMPTY \
 "uri\n" \
 "{\n" \
@@ -41,19 +127,8 @@ BOOST_AUTO_TEST_SUITE(uri_decode__invoke)
 #define URI_DECODE_STEALTH_ADDRESS \
 "uri\n" \
 "{\n" \
+"    address hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i\n" \
 "    scheme bitcoin\n" \
-"    stealth_address\n" \
-"    {\n" \
-"        encoded hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i\n" \
-"        prefix \"\"\n" \
-"        scan_public_key 031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\n" \
-"        signatures 1\n" \
-"        spend\n" \
-"        {\n" \
-"            public_key 031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\n" \
-"        }\n" \
-"        testnet false\n" \
-"    }\n" \
 "}\n"
 
 #define URI_DECODE_AMOUNT \
@@ -87,26 +162,15 @@ BOOST_AUTO_TEST_SUITE(uri_decode__invoke)
 #define URI_DECODE_COMPOSITE \
 "uri\n" \
 "{\n" \
+"    address hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i\n" \
 "    amount 10000120000\n" \
 "    label &=\\\\n#\n" \
 "    message \"hello bitcoin\"\n" \
 "    r http://example.com?purchase=shoes&user=bob\n" \
 "    scheme bitcoin\n" \
-"    stealth_address\n" \
-"    {\n" \
-"        encoded hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i\n" \
-"        prefix \"\"\n" \
-"        scan_public_key 031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\n" \
-"        signatures 1\n" \
-"        spend\n" \
-"        {\n" \
-"            public_key 031bab84e687e36514eeaf5a017c30d32c1f59dd4ea6629da7970ca374513dd006\n" \
-"        }\n" \
-"        testnet false\n" \
-"    }\n" \
 "}\n"
 
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__lower_case_scheme_empty_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__lower_case_scheme_empty__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "bitcoin:" });
@@ -114,16 +178,15 @@ BOOST_AUTO_TEST_CASE(uri_decode__invoke__lower_case_scheme_empty_uri__okay_outpu
     BX_REQUIRE_OUTPUT(URI_DECODE_EMPTY);
 }
 
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__upper_case_scheme_empty_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__upper_case_scheme_empty__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "BITCOIN:" });
     BX_REQUIRE_OKAY(command.invoke(output, error));
-    auto foo = output.str();
     BX_REQUIRE_OUTPUT(URI_DECODE_EMPTY);
 }
 
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__address_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__address__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "bitcoin:1HT7xU2Ngenf7D4yocz2SAcnNLW7rK8d4E" });
@@ -131,7 +194,7 @@ BOOST_AUTO_TEST_CASE(uri_decode__invoke__address_uri__okay_output)
     BX_REQUIRE_OUTPUT(URI_DECODE_ADDRESS);
 }
 
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__stealth_address_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__stealth_address__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "bitcoin:hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i" });
@@ -139,8 +202,16 @@ BOOST_AUTO_TEST_CASE(uri_decode__invoke__stealth_address_uri__okay_output)
     BX_REQUIRE_OUTPUT(URI_DECODE_STEALTH_ADDRESS);
 }
 
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__not_bitcoin__failure_error)
+{
+    BX_DECLARE_COMMAND(uri_decode);
+    command.set_uri_argument({ "http://eff.org" });
+    BX_REQUIRE_FAILURE(command.invoke(output, error));
+    BX_REQUIRE_ERROR(BX_URI_DECODE_NOT_BITCOIN "\n");
+}
+
 // only one amount token is allowed on a bitcoin uri
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__amount_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__amount__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "bitcoin:?amount=100.0012" });
@@ -149,7 +220,7 @@ BOOST_AUTO_TEST_CASE(uri_decode__invoke__amount_uri__okay_output)
 }
 
 // boost::property_tree info serialization is JSON-based, it escapes backslashes
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__label_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__label__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_format_option({ "info" });
@@ -158,7 +229,7 @@ BOOST_AUTO_TEST_CASE(uri_decode__invoke__label_uri__okay_output)
     BX_REQUIRE_OUTPUT(URI_DECODE_LABEL);
 }
 
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__message_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__message__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "bitcoin:?message=hello%20bitcoin" });
@@ -166,7 +237,7 @@ BOOST_AUTO_TEST_CASE(uri_decode__invoke__message_uri__okay_output)
     BX_REQUIRE_OUTPUT(URI_DECODE_MESSAGE);
 }
 
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__payment_request_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__payment_request__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "bitcoin:?r=http%3A%2F%2Fexample.com%3Fpurchase%3Dshoes%26user%3Dbob" });
@@ -174,7 +245,7 @@ BOOST_AUTO_TEST_CASE(uri_decode__invoke__payment_request_uri__okay_output)
     BX_REQUIRE_OUTPUT(URI_DECODE_PAYMENT_REQUEST);
 }
 
-BOOST_AUTO_TEST_CASE(uri_decode__invoke__stealth_composite_uri__okay_output)
+BOOST_AUTO_TEST_CASE(uri_decode__invoke__stealth_composite__okay_output)
 {
     BX_DECLARE_COMMAND(uri_decode);
     command.set_uri_argument({ "bItCoIn:hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i?amount=100.0012&label=%26%3D%5Cn%23&message=hello%20bitcoin&r=http%3A%2F%2Fexample.com%3Fpurchase%3Dshoes%26user%3Dbob" });
