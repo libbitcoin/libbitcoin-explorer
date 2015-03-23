48ee203bef3e6dcc5b47d4ab735e081a158d5147
evoskuil <eric@voskuil.org>
2015-03-22 22:36:12 -0700

    Use libbitcoin-build to generate Makefile.am (vs. model).

---
+++ model/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+###############################################################################
+# Copyright (c) 2014-2015 libbitcoin developers (see COPYING).
+#
+# Generate libbitcoin build artifacts from XML + GSL.
+#
+# This executes the iMatix GSL code generator.
+# See https://github.com/imatix/gsl for details.
+###############################################################################
+
+# Exit this script on the first build error.
+set -e
+
+# Do everything relative to this file location.
+cd `dirname "$0"`
+
+mkdir -p ../data
+mkdir -p ../src/commands
+mkdir -p ../include/bitcoin/explorer/commands
+
+gsl -q generate.xml
