60de193b100155dd3d4dd72357b731b739cef77c
Neill Miller <neillm@thecodefactory.org>
2015-04-02 22:04:42 -0500

    - initial addition of bip38 compatible ec-lock/ec-unlock implementation

--- include/bitcoin/explorer/commands/ec-unlock.hpp
+++ include/bitcoin/explorer/commands/ec-unlock.hpp
@@ -72,8 +72,8 @@ namespace commands {
 /**
  * Various localizable strings.
  */
-#define BX_EC_UNLOCK_NOT_IMPLEMENTED \
-    "This command is not yet implemented."
+#define BX_EC_UNLOCK_FAILURE \
+    "The specified input and passphrase combination is invalid."
 
 /**
  * Class to implement the ec-unlock command.
@@ -124,6 +124,7 @@ public:
     BCX_API virtual arguments_metadata& load_arguments()
     {
         return get_argument_metadata()
+            .add("SECRET", 1)
             .add("PASSPHRASE", 1);
     }
 
@@ -135,6 +136,7 @@ public:
     BCX_API virtual void load_fallbacks(std::istream& input, 
         po::variables_map& variables)
     {
+        load_input(get_secret_argument(), "SECRET", variables, input);
     }
 
     /**
@@ -158,6 +160,11 @@ public:
             "The path to the configuration settings file."
         )
         (
+            "SECRET",
+            value<primitives::base58>(&argument_.secret),
+            "The BIP38 Base58Check value to unlock. If not specified the value is read from STDIN."
+        )
+        (
             "PASSPHRASE",
             value<std::string>(&argument_.passphrase)->required(),
             "The passphrase."
@@ -176,6 +183,23 @@ public:
         std::ostream& cerr);
 
     /* Properties */
+
+    /**
+     * Get the value of the SECRET argument.
+     */
+    BCX_API virtual primitives::base58& get_secret_argument()
+    {
+        return argument_.secret;
+    }
+
+    /**
+     * Set the value of the SECRET argument.
+     */
+    BCX_API virtual void set_secret_argument(
+        const primitives::base58& value)
+    {
+        argument_.secret = value;
+    }
 
     /**
      * Get the value of the PASSPHRASE argument.
@@ -204,10 +228,12 @@ private:
     struct argument
     {
         argument()
-          : passphrase()
+          : secret(),
+            passphrase()
         {
         }
 
+        primitives::base58 secret;
         std::string passphrase;
     } argument_;
 
--- src/commands/ec-lock.cpp
+++ src/commands/ec-lock.cpp
@@ -21,56 +21,29 @@
 #include <bitcoin/explorer/commands/ec-lock.hpp>
 
 #include <iostream>
+#include <bitcoin/bitcoin.hpp>
 #include <bitcoin/explorer/define.hpp>
+#include <bitcoin/explorer/primitives/ec_private.hpp>
 
+using namespace bc;
+using namespace bc::bip38;
 using namespace bc::explorer;
 using namespace bc::explorer::commands;
+using namespace bc::explorer::primitives;
 
 console_result ec_lock::invoke(std::ostream& output, std::ostream& error)
 {
-    // Bound parameters.
-    //const auto& secret = get_ec_private_key_argument();
-    //const auto& passphrase = get_passphrase_argument();
+    const auto& secret = get_ec_private_key_argument();
+    const auto& passphrase = get_passphrase_argument();
 
-    // TODO: implement BIP38
+    if (!verify_private_key(secret))
+    {
+        error << BX_EC_LOCK_PRIVKEY_LENGTH_INVALID << std::endl;
+        return console_result::failure;
+    }
 
-    error << BX_EC_LOCK_NOT_IMPLEMENTED << std::endl;
-    return console_result::failure;
+    const auto locked = bip38_lock_secret(secret, passphrase);
+    output << encode_base58(locked) << std::endl;
+    return console_result::okay;
 }
 
-//#!/usr/bin/python
-//import  sys, subprocess, hashlib
-//PIPE = subprocess.PIPE
-//
-//def runcmd(args,inp=''):
-//    p = subprocess.Popen(args, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-//    return p.communicate(input=inp)[0]
-//
-//args, opts = [], {}
-//i = 1
-//while i < len(sys.argv):
-//    if sys.argv[i][0:2] == '--':
-//        opts[sys.argv[i][2:]] = sys.argv[i+1]
-//        i += 2
-//    else:
-//        args.append(sys.argv[i])
-//        i += 1
-//
-//if len(args) == 0: seed = ''
-//elif len(args) == 1: seed = args[0]
-//else: seed = args[0] + ':' + args[1]
-//
-//def bitcoin256(x): return hashlib.bitcoin256(x).base16digest()
-//
-//def slowsha(x,rounds=1000):
-//    old, new = x, ''
-//    for i in range(rounds):
-//        new = hashlib.bitcoin256(old).digest()
-//        old = new+x
-//    return new.encode('base16')
-//
-//if opts.get('algo','') == 'slowsha': algo = slowsha
-//elif opts.get('rounds',None): algo = lambda x: slowsha(x,int(opts['rounds']))
-//else: algo = bitcoin256
-//
-//print runcmd(['sx','base58check-encode',algo(seed),'128']).strip()
