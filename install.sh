700b72905bd31a2b7ab2a19d6ea91de27ab0c693
Phillip Mienk <mienkphi@gmail.com>
2015-04-21 00:54:44 -0700

    Regenerated .travis.yml

--- install.sh
+++ install.sh
@@ -216,6 +216,7 @@ BOOST_OPTIONS_GCC=\
 "--with-chrono "\
 "--with-date_time "\
 "--with-filesystem "\
+"--with-iostreams "\
 "--with-locale "\
 "--with-program_options "\
 "--with-regex "\
@@ -229,6 +230,7 @@ BOOST_OPTIONS_CLANG=\
 "--with-chrono "\
 "--with-date_time "\
 "--with-filesystem "\
+"--with-iostreams "\
 "--with-locale "\
 "--with-program_options "\
 "--with-regex "\
@@ -268,6 +270,20 @@ SECP256K1_OPTIONS=\
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
 "--without-tests "\
+"--without-examples "\
+"${with_boost} "\
+"${with_pkgconfigdir} "
+
+# Define protobuf options.
+#------------------------------------------------------------------------------
+PROTOBUF_OPTIONS=\
+"--enable-silent-rules "
+
+# Define bitcoin-protocol options.
+#------------------------------------------------------------------------------
+BITCOIN_PROTOCOL_OPTIONS=\
+"--without-tests "\
+"--without-examples "\
 "${with_boost} "\
 "${with_pkgconfigdir} "
 
@@ -602,6 +618,8 @@ build_all()
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
     build_from_github pmienk libbitcoin refactor_types $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github libbitcoin protobuf 2.6.0 $SEQUENTIAL "$@" $PROTOBUF_OPTIONS
+    build_from_github pmienk libbitcoin-protocol refactor_types $PARALLEL "$@" $BITCOIN_PROTOCOL_OPTIONS
     build_from_github pmienk libbitcoin-client refactor_types $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
     build_from_travis pmienk libbitcoin-explorer refactor_types $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
 }
