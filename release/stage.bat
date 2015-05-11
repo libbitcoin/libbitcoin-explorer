ba0c0de9ad497db1211d03b889f864955410d53e
evoskuil <eric@voskuil.org>
2015-05-10 18:58:28 -0700

    Remove obsolete utils.

--- release/stage.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-COPY   ..\bin\x64\Release\CTP_Nov2013\static\bx.exe  staging\bx-windows-x64-net.exe
-COPY ..\bin\Win32\Release\CTP_Nov2013\static\bx.exe  staging\bx-windows-x86-net.exe
-
-PAUSE
