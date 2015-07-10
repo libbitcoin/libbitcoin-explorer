a13fe2d42b2bb8d30d82e1b9525f4bbd3c0b264e
evoskuil <eric@voskuil.org>
2015-07-10 16:39:00 -0700

    Remove dead code.

--- src/primitives/btc256.cpp
+++ src/primitives/btc256.cpp
@@ -25,7 +25,6 @@
 #include <boost/program_options.hpp>
 #include <bitcoin/bitcoin.hpp>
 #include <bitcoin/explorer/define.hpp>
-#include <bitcoin/explorer/primitives/base16.hpp>
 
 using namespace po;
 
