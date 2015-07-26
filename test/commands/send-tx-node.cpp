ca574841f72673f4cc09d6ae3911cdbe18feef20
evoskuil <eric@voskuil.org>
2015-07-25 22:02:08 -0700

    Disable test (work in progress).

--- test/commands/send-tx-node.cpp
+++ test/commands/send-tx-node.cpp
@@ -25,18 +25,18 @@ BX_USING_NAMESPACES()
 BOOST_AUTO_TEST_SUITE(network)
 BOOST_AUTO_TEST_SUITE(send_tx_node__invoke)
 
-#define SEND_TX_NODE_A \
-"0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee097010000006a473044022039a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c202201035fe810e283bcf394485c6a9dfd117ad9f684cdd83d36453718f5d0491b9dd012103c40cbd64c9c608df2c9730f49b0888c4db1c436e8b2b74aead6c6afbd10428c0ffffffff01905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
-
-BOOST_AUTO_TEST_CASE(send_tx_node__invoke__mainnet_tx_A__failure_error)
-{
-    BX_DECLARE_COMMAND(send_tx_node);
-    command.set_host_option(BX_MAINNET_HOST);
-    command.set_port_option(BX_MAINNET_PORT);
-    command.set_transaction_argument({ SEND_TX_NODE_A });
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    //BX_REQUIRE_ERROR(BX_SERVICE_STOPPED_MESSAGE "\n");
-}
+//#define SEND_TX_NODE_A \
+//"0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee097010000006a473044022039a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c202201035fe810e283bcf394485c6a9dfd117ad9f684cdd83d36453718f5d0491b9dd012103c40cbd64c9c608df2c9730f49b0888c4db1c436e8b2b74aead6c6afbd10428c0ffffffff01905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
+//
+//BOOST_AUTO_TEST_CASE(send_tx_node__invoke__mainnet_tx_A__failure_error)
+//{
+//    BX_DECLARE_COMMAND(send_tx_node);
+//    command.set_host_option(BX_MAINNET_HOST);
+//    command.set_port_option(BX_MAINNET_PORT);
+//    command.set_transaction_argument({ SEND_TX_NODE_A });
+//    BX_REQUIRE_FAILURE(command.invoke(output, error));
+//    //BX_REQUIRE_ERROR(BX_SERVICE_STOPPED_MESSAGE "\n");
+//}
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
