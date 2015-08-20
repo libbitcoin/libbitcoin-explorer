2af6da643a7a892de159fda727168e212207e7c4
Neill Miller <neillm@thecodefactory.org>
2015-08-19 20:06:03 -0500

    - point to proper branch with code from pr in install.sh

--- install.sh
+++ install.sh
@@ -606,7 +606,7 @@ build_all()
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
     build_from_github libbitcoin libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
     build_from_github libbitcoin libbitcoin-client master $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
-    build_from_travis libbitcoin libbitcoin-explorer master $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
+    build_from_travis thecodefactory libbitcoin-explorer bip-38 $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
 }
 
 
