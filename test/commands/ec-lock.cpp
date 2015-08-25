c5feef9ab8f86b5784f611069ffda54168fd0fc3
Neill Miller <neillm@thecodefactory.org>
2015-08-24 20:47:39 -0500

    - remove any ec-lock/unlock test calls using passphrase if icu is not supported

--- test/commands/ec-lock.cpp
+++ test/commands/ec-lock.cpp
@@ -39,19 +39,15 @@ BOOST_AUTO_TEST_CASE(ec_lock__invoke__always__failure_error)
 #endif
 }
 
+#ifdef WITH_ICU
 BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_passphrase_1_no_compress__okay_if_icu)
 {
     BX_DECLARE_COMMAND(ec_lock);
     command.set_ec_private_key_argument({ "cbf4b9f70470856bb4f40f80b87edb90865997ffee6df315ab166d713af433a5" });
     command.set_passphrase_argument({ "TestingOneTwoThree" });
     command.set_compress_argument({ "0" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_LOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_passphrase_2_no_compress__okay_if_icu)
@@ -60,13 +56,8 @@ BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_passphrase_2_no_compress__okay_if_icu
     command.set_ec_private_key_argument({ "09c2686880095b1a4c249ee3ac4eea8a014f11e6f986d0b5025ac1f39afbd9ae" });
     command.set_passphrase_argument({ "Satoshi" });
     command.set_compress_argument({ "0" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTePPX1dWByq\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_LOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_passphrase_1_compress__okay_if_icu)
@@ -75,13 +66,8 @@ BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_passphrase_1_compress__okay_if_icu)
     command.set_ec_private_key_argument({ "cbf4b9f70470856bb4f40f80b87edb90865997ffee6df315ab166d713af433a5" });
     command.set_passphrase_argument({ "TestingOneTwoThree" });
     command.set_compress_argument({ "1" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("6PYNKZ1EAgYgmQfmNVamxyXVWHzK5s6DGhwP4J5o44cvXdoY7sRzhtpUeo\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_LOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_passphrase_2_compress__okay_if_icu)
@@ -90,14 +76,10 @@ BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_passphrase_2_compress__okay_if_icu)
     command.set_ec_private_key_argument({ "09c2686880095b1a4c249ee3ac4eea8a014f11e6f986d0b5025ac1f39afbd9ae" });
     command.set_passphrase_argument({ "Satoshi" });
     command.set_compress_argument({ "1" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("6PYLtMnXvfG3oJde97zRyLYFZCYizPU5T3LwgdYJz1fRhh16bU7u6PPmY7\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_LOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
+#endif // WITH_ICU
 
 BOOST_AUTO_TEST_CASE(ec_lock__invoke__lock_intermediate_1_no_compress__okay)
 {
--- test/commands/ec-unlock.cpp
+++ test/commands/ec-unlock.cpp
@@ -38,18 +38,16 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__always__failure_error)
 #endif
 }
 
+#ifdef WITH_ICU
+// NOTE: All ec-unlock tests require a passphrase which requires icu
+
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_1_no_compress__okay_if_icu)
 {
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PRVWUbkzzsbcVac2qwfssoUJAN1Xhrg6bNk8J7Nzm5H7kxEbn2Nh2ZoGg" });
     command.set_passphrase_argument({ "TestingOneTwoThree" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("cbf4b9f70470856bb4f40f80b87edb90865997ffee6df315ab166d713af433a5\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_2_no_compress__okay_if_icu)
@@ -57,13 +55,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_2_no_compress__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PRNFFkZc2NZ6dJqFfhRoFNMR9Lnyj7dYGrzdgXXVMXcxoKTePPX1dWByq" });
     command.set_passphrase_argument({ "Satoshi" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("09c2686880095b1a4c249ee3ac4eea8a014f11e6f986d0b5025ac1f39afbd9ae\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_1_compress__okay_if_icu)
@@ -71,13 +64,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_1_compress__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PYNKZ1EAgYgmQfmNVamxyXVWHzK5s6DGhwP4J5o44cvXdoY7sRzhtpUeo" });
     command.set_passphrase_argument({ "TestingOneTwoThree" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("cbf4b9f70470856bb4f40f80b87edb90865997ffee6df315ab166d713af433a5\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_2_compress__okay_if_icu)
@@ -85,13 +73,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_2_compress__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PYLtMnXvfG3oJde97zRyLYFZCYizPU5T3LwgdYJz1fRhh16bU7u6PPmY7" });
     command.set_passphrase_argument({ "Satoshi" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("09c2686880095b1a4c249ee3ac4eea8a014f11e6f986d0b5025ac1f39afbd9ae\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_1_ec_multiply__okay_if_icu)
@@ -99,13 +82,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_1_ec_multiply__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PfQu77ygVyJLZjfvMLyhLMQbYnu5uguoJJ4kMCLqWwPEdfpwANVS76gTX" });
     command.set_passphrase_argument({ "TestingOneTwoThree" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("a43a940577f4e97f5c4d39eb14ff083a98187c64ea7c99ef7ce460833959a519\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_2_ec_multiply__okay_if_icu)
@@ -113,13 +91,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_2_ec_multiply__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PfLGnQs6VZnrNpmVKfjotbnQuaJK4KZoPFrAjx1JMJUa1Ft8gnf5WxfKd" });
     command.set_passphrase_argument({ "Satoshi" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("c2c8036df268f498099350718c4a3ef3984d2be84618c2650f5171dcc5eb660a\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_3_ec_multiply__okay_if_icu)
@@ -127,13 +100,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_3_ec_multiply__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PgNBNNzDkKdhkT6uJntUXwwzQV8Rr2tZcbkDcuC9DZRsS6AtHts4Ypo1j" });
     command.set_passphrase_argument({ "MOLON LABE" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("44ea95afbf138356a05ea32110dfd627232d0f2991ad221187be356f19fa8190\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_4_ec_multiply__okay_if_icu)
@@ -141,13 +109,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_4_ec_multiply__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PgGWtx25kUg8QWvwuJAgorN6k9FbE25rv5dMRwu5SKMnfpfVe5mar2ngH" });
     command.set_passphrase_argument({ "ΜΟΛΩΝ ΛΑΒΕ" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("ca2759aa4adb0f96c414f36abeb8db59342985be9fa50faac228c8e7d90e3006\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_5_ec_multiply__okay_if_icu)
@@ -155,13 +118,8 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_5_ec_multiply__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PfPAw5HErFdzMyBvGMwSfSWjKmzgm3jDg7RxQyVCSSBJFZLAZ6hVupmpn" });
     command.set_passphrase_argument({ "libbitcoin test" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("fb4bfb0bfe151d524b0b11983b9f826d6a0bc7f7bdc480864a1b557ff0c59eb4\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
 
 BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_6_ec_multiply__okay_if_icu)
@@ -169,14 +127,11 @@ BOOST_AUTO_TEST_CASE(ec_unlock__invoke__unlock_6_ec_multiply__okay_if_icu)
     BX_DECLARE_COMMAND(ec_unlock);
     command.set_secret_argument({ "6PfU2yS6DUHjgH8wmsJRT1rHWXRofmDV5UJ3dypocew56BDcw5TQJXFYfm" });
     command.set_passphrase_argument({ "Libbitcoin BIP38 Test Vector" });
-#ifdef WITH_ICU
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("97c745cc980e5a070e12d0bff3f539b70748aadb406045fc1b42d4ded559a564\n");
-#else
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_EC_UNLOCK_USING_PASSPHRASE_UNSUPPORTED "\n");
-#endif
 }
+
+#endif // WITH_ICU
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
