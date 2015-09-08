c78dbc3218992df20fea8c91ee322a7073967154
evoskuil <eric@voskuil.org>
2015-09-08 15:05:32 -0700

    Implement full test matrix for 'token-new' (intermediate generation).

--- test/commands/token-new.cpp
+++ test/commands/token-new.cpp
@@ -24,8 +24,83 @@ BX_USING_NAMESPACES()
 BOOST_AUTO_TEST_SUITE(offline)
 BOOST_AUTO_TEST_SUITE(token_new__invoke)
 
-BOOST_AUTO_TEST_CASE(token_new__invoke__default__okay)
+BOOST_AUTO_TEST_CASE(token_new__invoke__empty_salt__failure_error)
 {
+    BX_DECLARE_COMMAND(token_new);
+    BX_REQUIRE_FAILURE(command.invoke(output, error));
+    BX_REQUIRE_ERROR(BX_TOKEN_NEW_SHORT_SALT "\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__3_byte_salt__failure_error)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_salt_argument({ "baadf0" });
+    BX_REQUIRE_FAILURE(command.invoke(output, error));
+    BX_REQUIRE_ERROR(BX_TOKEN_NEW_SHORT_SALT "\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__4_byte_salt__okay)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_salt_argument({ "baadf00d" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("passphrasecpXbDpHuo8F7yQVcg1eQKPuX7rzGwBtEH1YSZnKbyk75x3rugZu1ci4RyF4rEn\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__passphrase_8_byte_salt__okay)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_salt_argument({ "baadf00dbaadf00d" });
+    command.set_passphrase_argument("passphrase");
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("passphrasecpXbDpHuo8F7x4pQXMhsJs2j7L8LTV8ujk9jGqgzUrafBeto9VrabP5SmvANvz\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__excess_lot_4_byte_salt__okay)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_lot_option(1048575 + 1);
+    command.set_salt_argument({ "baadf00d" });
+    BX_REQUIRE_FAILURE(command.invoke(output, error));
+    BX_REQUIRE_ERROR(BX_TOKEN_NEW_MAXIMUM_LOT "\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__lot_4_byte_salt__okay)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_lot_option(1048575);
+    command.set_salt_argument({ "baadf00d" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("passphrasecpXbDpHuo8FGWjkFCxTEYSaekfi45D88ad5DVReNLhCTbnETGLfYfcrV4vwx3Q\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__excess_sequence_4_byte_salt__okay)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_sequence_option(4095 + 1);
+    command.set_salt_argument({ "baadf00d" });
+    BX_REQUIRE_FAILURE(command.invoke(output, error));
+    BX_REQUIRE_ERROR(BX_TOKEN_NEW_MAXIMUM_SEQUENCE "\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__sequence_4_byte_salt__okay)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_sequence_option(4095);
+    command.set_salt_argument({ "baadf00d" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("passphrasecpXbDpHuo8FGWjkFCxTEYSaekfi45D88ad5DVReNLhCTbnETGLfYfcrV4vwx3Q\n");
+}
+
+BOOST_AUTO_TEST_CASE(token_new__invoke__lot_sequence_passphrase_4_byte_salt__okay)
+{
+    BX_DECLARE_COMMAND(token_new);
+    command.set_lot_option(1048575);
+    command.set_sequence_option(4095);
+    command.set_salt_argument({ "baadf00d" });
+    command.set_passphrase_argument("passphrase");
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("passphrasecpXbDpHuo8FGWnwMTnTFiHSDnqyARArE2YSFQzMHtCZvM2oWg2K3Ua2crKyc11\n");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
