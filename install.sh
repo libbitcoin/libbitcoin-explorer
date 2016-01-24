a1146e0177f15132279e13c4e9f9201570d4f4d6
evoskuil <eric@voskuil.org>
2016-01-24 10:53:17 -0800

    Restore disable of czmq_selftest switch.

--- install.sh
+++ install.sh
@@ -313,6 +313,7 @@ ZMQ_OPTIONS=\
 #------------------------------------------------------------------------------
 CZMQ_OPTIONS=\
 "--disable-zmakecert "\
+"--disable-czmq_selftest "\
 "${with_pkgconfigdir} "
 
 # Define czmqpp options.
