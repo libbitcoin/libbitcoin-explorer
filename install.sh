cf5a93649cdd8709b489f160a9d7b6ec85af3861
evoskuil <eric@voskuil.org>
2016-02-04 16:18:20 -0800

    Build from master vs. sync.

--- install.sh
+++ install.sh
@@ -620,10 +620,10 @@ build_all()
     build_from_github zeromq czmq master $PARALLEL "$@" $CZMQ_OPTIONS
     build_from_github zeromq czmqpp master $PARALLEL "$@" $CZMQPP_OPTIONS
     build_from_github libbitcoin secp256k1 version4 $PARALLEL "$@" $SECP256K1_OPTIONS
-    build_from_github libbitcoin libbitcoin sync $PARALLEL "$@" $BITCOIN_OPTIONS
-    build_from_github libbitcoin libbitcoin-client sync $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
+    build_from_github libbitcoin libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_github libbitcoin libbitcoin-client master $PARALLEL "$@" $BITCOIN_CLIENT_OPTIONS
     build_from_github libbitcoin libbitcoin-network master $PARALLEL "$@" $BITCOIN_NETWORK_OPTIONS
-    build_from_travis libbitcoin libbitcoin-explorer sync $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
+    build_from_travis libbitcoin libbitcoin-explorer master $PARALLEL "$@" $BITCOIN_EXPLORER_OPTIONS
 }
 
 
