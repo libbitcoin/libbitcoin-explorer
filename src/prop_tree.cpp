6b752e4d985ab7bd86505c059c941221e8ec6b33
Phillip Mienk <mienkphi@gmail.com>
2016-11-12 20:08:32 -0800

    Correct build break on pull request.

--- src/prop_tree.cpp
+++ src/prop_tree.cpp
@@ -222,7 +222,7 @@ ptree prop_list(const tx_output_type& tx_output)
         tree.put("address", address);
 
     tree.put("script", tx_output.script().to_string(
-        chain::rule_fork::all_rules));
+        machine::rule_fork::all_rules));
 
     // TODO: this will eventually change due to privacy problems, see:
     // lists.dyne.org/lurker/message/20140812.214120.317490ae.en.html
