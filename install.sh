a88dc3ff350308b6fef0af5638c9713342901792
Neill Miller <neillm@thecodefactory.org>
2017-01-02 18:01:12 -0600

    Updates installer zlib download URL

--- install.sh
+++ install.sh
@@ -41,13 +41,13 @@ ICU_ARCHIVE="icu4c-55_1-src.tgz"
 
 # ZLib archive.
 #------------------------------------------------------------------------------
-ZLIB_URL="http://zlib.net/zlib-1.2.8.tar.xz"
-ZLIB_ARCHIVE="zlib-1.2.8.tar.xz"
+ZLIB_URL="https://github.com/madler/zlib/archive/v1.2.9.tar.gz"
+ZLIB_ARCHIVE="v1.2.9.tar.gz"
 
 # PNG archive.
 #------------------------------------------------------------------------------
-PNG_URL="http://downloads.sourceforge.net/project/libpng/libpng16/1.6.25/libpng-1.6.25.tar.xz"
-PNG_ARCHIVE="libpng-1.6.25.tar.xz"
+PNG_URL="http://downloads.sourceforge.net/project/libpng/libpng16/1.6.27/libpng-1.6.27.tar.xz"
+PNG_ARCHIVE="libpng-1.6.27.tar.xz"
 
 # QREncode archive.
 #------------------------------------------------------------------------------
@@ -728,7 +728,7 @@ build_from_travis()
 build_all()
 {
     build_from_tarball $ICU_URL $ICU_ARCHIVE gzip source $PARALLEL "$BUILD_ICU" "${ICU_OPTIONS[@]}" "$@"
-    build_from_tarball $ZLIB_URL $ZLIB_ARCHIVE xz . $PARALLEL "$BUILD_ZLIB" "${ZLIB_OPTIONS[@]}" "$@"
+    build_from_tarball $ZLIB_URL $ZLIB_ARCHIVE gzip . $PARALLEL "$BUILD_ZLIB" "${ZLIB_OPTIONS[@]}" "$@"
     build_from_tarball $PNG_URL $PNG_ARCHIVE xz . $PARALLEL "$BUILD_PNG" "${PNG_OPTIONS[@]}" "$@"
     build_from_tarball $QRENCODE_URL $QRENCODE_ARCHIVE bzip2 . $PARALLEL "$BUILD_QRENCODE" "${QRENCODE_OPTIONS[@]}" "$@"
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE bzip2 . $PARALLEL "$BUILD_BOOST" "${BOOST_OPTIONS[@]}"
