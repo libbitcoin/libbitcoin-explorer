1f6e1b9a25ad36d7c11b3b8900954d7e44dada2d
evoskuil <eric@voskuil.org>
2017-01-08 01:50:32 -0800

    Simplify tx encode-decode test matrix.

--- test/commands/tx-decode.cpp
+++ test/commands/tx-decode.cpp
@@ -25,14 +25,10 @@ BX_USING_NAMESPACES()
 BOOST_AUTO_TEST_SUITE(offline)
 BOOST_AUTO_TEST_SUITE(tx_decode__invoke)
 
-// Vector: sx.dyne.org/offlinetx.html
-
-#define TX_DECODE_TX_A_BASE16 \
-"0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000ffffffff01905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
-#define TX_DECODE_TX_A_INFO \
+#define TX_DECODE_EXAMPLE1 \
 "transaction\n" \
 "{\n" \
-"    hash 39d2caaf112f21364be00c5d0b14e3763468934e586e416bf74647a1906b18e0\n" \
+"    hash f9be6abf60342de5606421c7deaaf2d3f7133490db5242e8507e05926b16d090\n" \
 "    inputs\n" \
 "    {\n" \
 "        input\n" \
@@ -40,7 +36,7 @@ BOOST_AUTO_TEST_SUITE(tx_decode__invoke)
 "            previous_output\n" \
 "            {\n" \
 "                hash 97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3\n" \
-"                index 1\n" \
+"                index 0\n" \
 "            }\n" \
 "            script \"\"\n" \
 "            sequence 4294967295\n" \
@@ -51,35 +47,69 @@ BOOST_AUTO_TEST_SUITE(tx_decode__invoke)
 "    {\n" \
 "        output\n" \
 "        {\n" \
-"            address 13Ft7SkreJY9D823NPm4t6D1cBqLYTJtAe\n" \
-"            script \"dup hash160 [18c0bd8d1818f1bf99cb1df2269c645318ef7b73] equalverify checksig\"\n" \
-"            value 90000\n" \
+"            address 1966U1pjj15tLxPXZ19U48c99EJDkdXeqb\n" \
+"            script \"dup hash160 [58b7a60f11a904feef35a639b6048de8dd4d9f1c] equalverify checksig\"\n" \
+"            value 45000\n" \
 "        }\n" \
 "    }\n" \
 "    version 1\n" \
 "}\n"
-#define TX_DECODE_TX_A_XML \
-"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" \
-"<transaction><hash>39d2caaf112f21364be00c5d0b14e3763468934e586e416bf74647a1906b18e0</hash><inputs><input><previous_output><hash>97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3</hash><index>1</index></previous_output><script/><sequence>4294967295</sequence></input></inputs><lock_time>0</lock_time><outputs><output><address>13Ft7SkreJY9D823NPm4t6D1cBqLYTJtAe</address><script>dup hash160 [18c0bd8d1818f1bf99cb1df2269c645318ef7b73] equalverify checksig</script><value>90000</value></output></outputs><version>1</version></transaction>\n"
 
-#define TX_DECODE_TX_B_BASE16 \
-"0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee097010000006a473044022039a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c202201035fe810e283bcf394485c6a9dfd117ad9f684cdd83d36453718f5d0491b9dd012103c40cbd64c9c608df2c9730f49b0888c4db1c436e8b2b74aead6c6afbd10428c0ffffffff01905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
-#define TX_DECODE_TX_B_INFO \
+#define TX_DECODE_EXAMPLE2 \
 "transaction\n" \
 "{\n" \
-"    hash c3350a915979f8296675115f4e579c2a1aacc08074249584f794e1dfbda3609b\n" \
+"    hash 4a013715c2ef8ddeae2792eea4751565acd1ad5ed27542d45f2ebe571f4899e9\n" \
 "    inputs\n" \
 "    {\n" \
 "        input\n" \
 "        {\n" \
-"            address 1DRCR5Gvg18FNo8HUPH1u5bfd9b5XfnJDj\n" \
 "            previous_output\n" \
 "            {\n" \
 "                hash 97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3\n" \
 "                index 1\n" \
 "            }\n" \
-"            script \"[3044022039a36013301597daef41fbe593a02cc513d0b55527ec2df1050e2e8ff49c85c202201035fe810e283bcf394485c6a9dfd117ad9f684cdd83d36453718f5d0491b9dd01] [03c40cbd64c9c608df2c9730f49b0888c4db1c436e8b2b74aead6c6afbd10428c0]\"\n" \
-"            sequence 4294967295\n" \
+"            script \"\"\n" \
+"            sequence 7\n" \
+"        }\n" \
+"    }\n" \
+"    lock_time 0\n" \
+"    outputs\n" \
+"    {\n" \
+"        output\n" \
+"        {\n" \
+"            script \"return [14576f496f20b0befe21f39f765e81543ebd1790ec4a03d1b5a1c2e912749d90d0fd7b16322749e301a2b0dbfe278509011564590412b277]\"\n" \
+"            stealth\n" \
+"            {\n" \
+"                prefix 2927803294\n" \
+"                ephemeral_public_key 0214576f496f20b0befe21f39f765e81543ebd1790ec4a03d1b5a1c2e912749d90\n" \
+"            }\n" \
+"            value 0\n" \
+"        }\n" \
+"        output\n" \
+"        {\n" \
+"            address 1KbjyvFBRc2p6dKpTfDAFdT5DqmVLGX3B4\n" \
+"            script \"dup hash160 [cc04492c12d0ddeb4cf88cfccb0d6d78d0fcd39d] equalverify checksig\"\n" \
+"            value 42\n" \
+"        }\n" \
+"    }\n" \
+"    version 1\n" \
+"}\n"
+
+#define TX_DECODE_EXAMPLE3 \
+"transaction\n" \
+"{\n" \
+"    hash bfe73280b111a7dae1714b1efe869c0d0c854dd9d1c3ba51fa439e7fb4d0e63c\n" \
+"    inputs\n" \
+"    {\n" \
+"        input\n" \
+"        {\n" \
+"            previous_output\n" \
+"            {\n" \
+"                hash 97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3\n" \
+"                index 1\n" \
+"            }\n" \
+"            script \"\"\n" \
+"            sequence 7\n" \
 "        }\n" \
 "    }\n" \
 "    lock_time 0\n" \
@@ -89,47 +119,77 @@ BOOST_AUTO_TEST_SUITE(tx_decode__invoke)
 "        {\n" \
 "            address 13Ft7SkreJY9D823NPm4t6D1cBqLYTJtAe\n" \
 "            script \"dup hash160 [18c0bd8d1818f1bf99cb1df2269c645318ef7b73] equalverify checksig\"\n" \
-"            value 90000\n" \
+"            value 500\n" \
 "        }\n" \
 "    }\n" \
 "    version 1\n" \
 "}\n"
 
-BOOST_AUTO_TEST_CASE(tx_decode__invoke__tx_A_info__okay_output)
+#define TX_DECODE_EXAMPLE4 \
+"transaction\n" \
+"{\n" \
+"    hash 37c9c4ee0e84c7c7924f74d92cf0779ec6e8fc4c57ebab2593562d52c61c5eb8\n" \
+"    inputs\n" \
+"    {\n" \
+"        input\n" \
+"        {\n" \
+"            address 1JziqzXeBPyHPeAHrG4DCDW4ASXeGGF6p6\n" \
+"            previous_output\n" \
+"            {\n" \
+"                hash 7c3e880e7c93a7b01506188c36a239f70b561dfa622d0aa0d8f3b7403c94017d\n" \
+"                index 0\n" \
+"            }\n" \
+"            script \"[30450221008f66d188c664a8088893ea4ddd9689024ea5593877753ecc1e9051ed58c15168022037109f0d06e6068b7447966f751de8474641ad2b15ec37f4a9d159b02af6817401] [03e208f5403383c77d5832a268c9f71480f6e7bfbdfa44904becacfad66163ea31]\"\n" \
+"            sequence 4294967295\n" \
+"        }\n" \
+"    }\n" \
+"    lock_time 0\n" \
+"    outputs\n" \
+"    {\n" \
+"        output\n" \
+"        {\n" \
+"            address 1966U1pjj15tLxPXZ19U48c99EJDkdXeqb\n" \
+"            script \"dup hash160 [58b7a60f11a904feef35a639b6048de8dd4d9f1c] equalverify checksig\"\n" \
+"            value 45000\n" \
+"        }\n" \
+"    }\n" \
+"    version 1\n" \
+"}\n"
+
+BOOST_AUTO_TEST_CASE(tx_decode__invoke__example_1__expected_output)
 {
     BX_DECLARE_COMMAND(tx_decode);
     command.set_format_option({ "info" });
-    command.set_transaction_argument({ TX_DECODE_TX_A_BASE16 });
+    command.set_transaction_argument({ "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970000000000ffffffff01c8af0000000000001976a91458b7a60f11a904feef35a639b6048de8dd4d9f1c88ac00000000" });
     BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_DECODE_TX_A_INFO);
+    BX_REQUIRE_OUTPUT(TX_DECODE_EXAMPLE1);
 }
 
-// JSON encoding varies subtly by boost or platform, so this is disabled.
-//BOOST_AUTO_TEST_CASE(tx_decode__invoke__tx_A_json__okay_output)
-//{
-//    BX_DECLARE_COMMAND(tx_decode);
-//    command.set_format_option({ "json" });
-//    command.set_transaction_argument({ TX_DECODE_TX_A_NATIVE });
-//    BX_REQUIRE_OKAY(command.invoke(output, error));
-//    BX_REQUIRE_OUTPUT(TX_DECODE_TX_A_JSON);
-//}
-
-BOOST_AUTO_TEST_CASE(tx_decode__invoke__tx_A_xml__okay_output)
-{
-    BX_DECLARE_COMMAND(tx_decode);
-    command.set_format_option({ "xml" });
-    command.set_transaction_argument({ TX_DECODE_TX_A_BASE16 });
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_DECODE_TX_A_XML);
-}
-
-BOOST_AUTO_TEST_CASE(tx_decode__invoke__tx_B_info__okay_output)
+BOOST_AUTO_TEST_CASE(tx_decode__invoke__example_2__expected_output)
 {
     BX_DECLARE_COMMAND(tx_decode);
     command.set_format_option({ "info" });
-    command.set_transaction_argument({ TX_DECODE_TX_B_BASE16 });
+    command.set_transaction_argument({ "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000070000000200000000000000003a6a3814576f496f20b0befe21f39f765e81543ebd1790ec4a03d1b5a1c2e912749d90d0fd7b16322749e301a2b0dbfe278509011564590412b2772a000000000000001976a914cc04492c12d0ddeb4cf88cfccb0d6d78d0fcd39d88ac00000000" });
     BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_DECODE_TX_B_INFO);
+    BX_REQUIRE_OUTPUT(TX_DECODE_EXAMPLE2);
+}
+
+BOOST_AUTO_TEST_CASE(tx_decode__invoke__example_3__expected_output)
+{
+    BX_DECLARE_COMMAND(tx_decode);
+    command.set_format_option({ "info" });
+    command.set_transaction_argument({ "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee09701000000000700000001f4010000000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(TX_DECODE_EXAMPLE3);
+}
+
+BOOST_AUTO_TEST_CASE(tx_decode__invoke__example_4__expected_output)
+{
+    BX_DECLARE_COMMAND(tx_decode);
+    command.set_format_option({ "info" });
+    command.set_transaction_argument({ "01000000017d01943c40b7f3d8a00a2d62fa1d560bf739a2368c180615b0a7937c0e883e7c000000006b4830450221008f66d188c664a8088893ea4ddd9689024ea5593877753ecc1e9051ed58c15168022037109f0d06e6068b7447966f751de8474641ad2b15ec37f4a9d159b02af68174012103e208f5403383c77d5832a268c9f71480f6e7bfbdfa44904becacfad66163ea31ffffffff01c8af0000000000001976a91458b7a60f11a904feef35a639b6048de8dd4d9f1c88ac00000000" });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT(TX_DECODE_EXAMPLE4);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- test/commands/tx-encode.cpp
+++ test/commands/tx-encode.cpp
@@ -25,121 +25,52 @@ BX_USING_NAMESPACES()
 BOOST_AUTO_TEST_SUITE(offline)
 BOOST_AUTO_TEST_SUITE(tx_encode__invoke)
 
-// Vector: from script-decode
-#define TX_ENCODE_OUTPUT_SCRIPT "76a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac:500"
-#define TX_ENCODE_TX_SCRIPT "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee09701000000000700000001f4010000000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
-
-// Vector: sx.dyne.org/offlinetx.html
-#define TX_ENCODE_INPUT_A "97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3:1:7"
-#define TX_ENCODE_INPUT_B "97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3:1"
-#define TX_ENCODE_PAYMENT_ADDRESS_OUTPUT "13Ft7SkreJY9D823NPm4t6D1cBqLYTJtAe:90000"
-#define TX_ENCODE_TX_BV1_BASE16 "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000ffffffff0000000000"
-#define TX_ENCODE_TX_BCV1_BASE16 "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000ffffffff01905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
-#define TX_ENCODE_TX_BCV42_BASE16 "2a00000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000ffffffff01905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000"
-#define TX_ENCODE_TX_ACV1L42_BASE16 "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee09701000000000700000001905f0100000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac2a000000"
-
-// Vector: from stealth-encode tests.
-#define TX_ENCODE_SEED "baadf00dbaadf00dbaadf00dbaadf00d"
-#define TX_ENCODE_STEALTH_ADDRESS_A "hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i"
-#define TX_ENCODE_STEALTH_OUTPUT_A TX_ENCODE_STEALTH_ADDRESS_A ":42:" TX_ENCODE_SEED
-//#define TX_ENCODE_TX_STEALTH_A "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000070000000200000000000000003a6a3814576f496f20b0befe21f39f765e81543ebd1790ec4a03d1b5a1c2e912749d90d0fd7b16322749e301a2b0dbfe27850901156459000000002a000000000000001976a914cc04492c12d0ddeb4cf88cfccb0d6d78d0fcd39d88ac00000000"
-#define TX_ENCODE_TX_STEALTH_A "0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000070000000200000000000000003a6a3814576f496f20b0befe21f39f765e81543ebd1790ec4a03d1b5a1c2e912749d90d0fd7b16322749e301a2b0dbfe278509011564590412b2772a000000000000001976a914cc04492c12d0ddeb4cf88cfccb0d6d78d0fcd39d88ac00000000"
-
-BOOST_AUTO_TEST_SUITE(tx_encode__invoke__no_output)
-
-BOOST_AUTO_TEST_CASE(tx_encode__invoke__one_input_no_output_version_1__okay_output)
+BOOST_AUTO_TEST_CASE(tx_encode__invoke__locktime_conflict__locktime_conflict_error)
 {
     BX_DECLARE_COMMAND(tx_encode);
     command.set_version_option(1);
-    command.set_script_version_option(5);
-    command.set_inputs_option({ { TX_ENCODE_INPUT_B } });
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_ENCODE_TX_BV1_BASE16 "\n");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(tx_encode__invoke__script_output)
-
-BOOST_AUTO_TEST_CASE(tx_encode__invoke__one_input_script_output_version_1__okay_output)
-{
-    BX_DECLARE_COMMAND(tx_encode);
-    command.set_version_option(1);
-    command.set_script_version_option(43);
-    command.set_inputs_option({ { TX_ENCODE_INPUT_A } });
-    command.set_outputs_option({ { TX_ENCODE_OUTPUT_SCRIPT } });
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_ENCODE_TX_SCRIPT "\n");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(tx_encode__invoke__payment_address_output)
-
-BOOST_AUTO_TEST_CASE(tx_encode__invoke__one_input_payment_address_output_version_1__okay_output)
-{
-    BX_DECLARE_COMMAND(tx_encode);
-    command.set_version_option(1);
-    command.set_script_version_option(5);
-    command.set_inputs_option({ { TX_ENCODE_INPUT_B } });
-    command.set_outputs_option({ { TX_ENCODE_PAYMENT_ADDRESS_OUTPUT } });
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_ENCODE_TX_BCV1_BASE16 "\n");
-}
-
-BOOST_AUTO_TEST_CASE(tx_encode__invoke__one_input_payment_address__output_version_42__okay_output)
-{
-    BX_DECLARE_COMMAND(tx_encode);
-    command.set_version_option(42);
-    command.set_script_version_option(5);
-    command.set_inputs_option({ { TX_ENCODE_INPUT_B } });
-    command.set_outputs_option({ { TX_ENCODE_PAYMENT_ADDRESS_OUTPUT } });
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_ENCODE_TX_BCV42_BASE16 "\n");
-}
-
-BOOST_AUTO_TEST_CASE(tx_encode__invoke__one_input_payment_address__output_version_1_locktime_42__failure_error)
-{
-    BX_DECLARE_COMMAND(tx_encode);
-    command.set_version_option(1);
-    command.set_script_version_option(5);
     command.set_lock_time_option(42);
-    command.set_inputs_option({ { TX_ENCODE_INPUT_B } });
-    command.set_outputs_option({ { TX_ENCODE_PAYMENT_ADDRESS_OUTPUT } });
+    command.set_script_version_option(5);
+    command.set_inputs_option({ { "97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3:1" } });
+    command.set_outputs_option({ { "13Ft7SkreJY9D823NPm4t6D1cBqLYTJtAe:90000" } });
     BX_REQUIRE_FAILURE(command.invoke(output, error));
     BX_REQUIRE_ERROR(BX_TX_ENCODE_LOCKTIME_CONFLICT "\n");
 }
 
-BOOST_AUTO_TEST_CASE(tx_encode__invoke__one_input_payment_address__output_version_1_locktime_42_sequence_7__okay_output)
+BOOST_AUTO_TEST_CASE(tx_encode__invoke__example1__expected_output)
 {
     BX_DECLARE_COMMAND(tx_encode);
     command.set_version_option(1);
     command.set_script_version_option(5);
-    command.set_lock_time_option(42);
-    command.set_inputs_option({ { TX_ENCODE_INPUT_A } });
-    command.set_outputs_option({ { TX_ENCODE_PAYMENT_ADDRESS_OUTPUT } });
+    command.set_inputs_option({ { "97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3:0" } });
+    command.set_outputs_option({ { "1966U1pjj15tLxPXZ19U48c99EJDkdXeqb:45000" } });
     BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_ENCODE_TX_ACV1L42_BASE16 "\n");
+    BX_REQUIRE_OUTPUT("0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970000000000ffffffff01c8af0000000000001976a91458b7a60f11a904feef35a639b6048de8dd4d9f1c88ac00000000" "\n");
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(tx_encode__invoke__stealth_address_output)
-
-// TODO: add stealth filter test
-
-BOOST_AUTO_TEST_CASE(tx_encode__invoke__one_input_stealth_output_no_filter_version_1__okay_output)
+// Vector: from stealth-encode tests.
+BOOST_AUTO_TEST_CASE(tx_encode__invoke__example2__okay_output)
 {
     BX_DECLARE_COMMAND(tx_encode);
     command.set_version_option(1);
     command.set_script_version_option(5);
-    command.set_inputs_option({ { TX_ENCODE_INPUT_A } });
-    command.set_outputs_option({ { TX_ENCODE_STEALTH_OUTPUT_A } });
+    command.set_inputs_option({ { "97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3:1:7" } });
+    command.set_outputs_option({ { "hfFGUXFPKkQ5M6LC6aEUKMsURdhw93bUdYdacEtBA8XttLv7evZkira2i:42:baadf00dbaadf00dbaadf00dbaadf00d" } });
     BX_REQUIRE_OKAY(command.invoke(output, error));
-    BX_REQUIRE_OUTPUT(TX_ENCODE_TX_STEALTH_A "\n");
+    BX_REQUIRE_OUTPUT("0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee0970100000000070000000200000000000000003a6a3814576f496f20b0befe21f39f765e81543ebd1790ec4a03d1b5a1c2e912749d90d0fd7b16322749e301a2b0dbfe278509011564590412b2772a000000000000001976a914cc04492c12d0ddeb4cf88cfccb0d6d78d0fcd39d88ac00000000" "\n");
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// Vector: script from script-decode
+BOOST_AUTO_TEST_CASE(tx_encode__invoke__example3__okay_output)
+{
+    BX_DECLARE_COMMAND(tx_encode);
+    command.set_version_option(1);
+    command.set_script_version_option(5);
+    command.set_inputs_option({ { "97e06e49dfdd26c5a904670971ccf4c7fe7d9da53cb379bf9b442fc9427080b3:1:7" } });
+    command.set_outputs_option({ { "76a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac:500" } });
+    BX_REQUIRE_OKAY(command.invoke(output, error));
+    BX_REQUIRE_OUTPUT("0100000001b3807042c92f449bbf79b33ca59d7dfec7f4cc71096704a9c526dddf496ee09701000000000700000001f4010000000000001976a91418c0bd8d1818f1bf99cb1df2269c645318ef7b7388ac00000000" "\n");
+}
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
