e6cc02ffd7ce01b047129f2a3c0c09bb7d51afc1
evoskuil <eric@voskuil.org>
2017-03-06 11:22:28 -0800

    Update watch-address notification handler confirm to client changes.

--- src/commands/watch-address.cpp
+++ src/commands/watch-address.cpp
@@ -81,11 +81,15 @@ console_result watch_address::invoke(std::ostream& output, std::ostream& error)
     // This enables json-style array formatting.
     const auto json = encoding == encoding_engine::json;
 
-    auto on_update = [&output, &state, json](const payment_address& address,
-        size_t, const hash_digest& block_hash, const tx_type& tx)
+    auto on_update = [&output, &state, &address, json](const code& error,
+        uint16_t /*sequence*/, size_t /*height*/,
+        const hash_digest& block_hash, const tx_type& tx)
     {
-        state.output(prop_tree(tx, block_hash, address, json));
-        output << std::endl;
+        if (state.succeeded(error))
+        {
+            state.output(prop_tree(tx, block_hash, address, json));
+            output << std::endl;
+        }
     };
 
     client.set_on_update(on_update);
