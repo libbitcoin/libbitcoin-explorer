06fd1d91fa5fe6768c450913c8668543658c6e6e
evoskuil <eric@voskuil.org>
2017-12-11 00:06:12 -0800

    Style: 4 part test names.

--- test/commands/electrum-to-seed.cpp
+++ test/commands/electrum-to-seed.cpp
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE(electrum_to_seed__invoke__okay_output)
     BX_REQUIRE_OUTPUT("c4033901dd61ba26cfd0a1cf1ceb4b347606635aa3cb951eb6e819d58beedc04dd400a2e600d783c83c75879d6538abeecc7bb1b292b2a4d775d348d5d686427\n");
 }
 
-BOOST_AUTO_TEST_CASE(electrum_to_seed__invoke__dictionary_prefix_okay_output)
+BOOST_AUTO_TEST_CASE(electrum_to_seed__invoke__language_en__okay_output)
 {
     BX_DECLARE_COMMAND(electrum_to_seed);
     command.set_language_option({ "en" });
@@ -42,7 +42,7 @@ BOOST_AUTO_TEST_CASE(electrum_to_seed__invoke__dictionary_prefix_okay_output)
     BX_REQUIRE_OUTPUT("741b72fd15effece6bfe5a26a52184f66811bd2be363190e07a42cca442b1a5bb22b3ad0eb338197287e6d314866c7fba863ac65d3f156087a5052ebc7157fce\n");
 }
 
-BOOST_AUTO_TEST_CASE(electrum_to_seed__invoke__okay_output2)
+BOOST_AUTO_TEST_CASE(electrum_to_seed__invoke__12_words__okay_output)
 {
     BX_DECLARE_COMMAND(electrum_to_seed);
     command.set_language_option({ "en" });
@@ -51,6 +51,10 @@ BOOST_AUTO_TEST_CASE(electrum_to_seed__invoke__okay_output2)
     BX_REQUIRE_OKAY(command.invoke(output, error));
     BX_REQUIRE_OUTPUT("eb095e70f0eb8d24f2cef276c75b136c2ff41eb72a9291eb22ba45d11fbb875a965bd6ae09873543eea98bf3ae05067203cd56388e0978aebc3a511932704d80\n");
 }
+
+#ifdef WITH_ICU
+    // TODO: add passphrase tests.
+#endif
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
