eb6b3b1d47d238f3e18932b0277da17f71558465
evoskuil <eric@voskuil.org>
2018-01-02 01:25:23 -0800

    Standardize header.

--- test/commands/command.hpp
+++ test/commands/command.hpp
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef SETUP_HPP
-#define SETUP_HPP
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
 
 #include <iostream>
 #include <locale>
