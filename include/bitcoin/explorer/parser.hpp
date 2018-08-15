5fa6aad5bde1ee8b406611c2c6b26fcf6fdd7e7c
Phillip Mienk <mienkphi@gmail.com>
2018-08-14 21:43:16 -0700

    Add comment on -Woverloaded-virtual warning alteration.

--- include/bitcoin/explorer/parser.hpp
+++ include/bitcoin/explorer/parser.hpp
@@ -60,6 +60,7 @@ protected:
     virtual void load_command_variables(variables_map& variables,
         std::istream& input, int argc, const char* argv[]);
 
+    // Expose polymorphic load_command_variables method from base.
     using bc::config::parser::load_command_variables;
 
 private:
