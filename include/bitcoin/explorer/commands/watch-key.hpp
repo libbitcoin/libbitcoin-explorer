85fdf0664cacb8747fe162c64bc59b6ec337214b
Neill Miller <neillm@thecodefactory.org>
2019-06-04 13:02:19 -0400

    Add missing header file.

---
+++ include/bitcoin/explorer/commands/watch-key.hpp
@@ -0,0 +1,255 @@
+/**
+ * Copyright (c) 2011-2019 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef BX_WATCH_KEY_HPP
+#define BX_WATCH_KEY_HPP
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/program_options.hpp>
+#include <bitcoin/system.hpp>
+#include <bitcoin/explorer/command.hpp>
+#include <bitcoin/explorer/define.hpp>
+#include <bitcoin/explorer/generated.hpp>
+#include <bitcoin/explorer/config/address.hpp>
+#include <bitcoin/explorer/config/algorithm.hpp>
+#include <bitcoin/explorer/config/btc.hpp>
+#include <bitcoin/explorer/config/byte.hpp>
+#include <bitcoin/explorer/config/cert_key.hpp>
+#include <bitcoin/explorer/config/ec_private.hpp>
+#include <bitcoin/explorer/config/electrum.hpp>
+#include <bitcoin/explorer/config/encoding.hpp>
+#include <bitcoin/explorer/config/endorsement.hpp>
+#include <bitcoin/explorer/config/hashtype.hpp>
+#include <bitcoin/explorer/config/hd_key.hpp>
+#include <bitcoin/explorer/config/language.hpp>
+#include <bitcoin/explorer/config/raw.hpp>
+#include <bitcoin/explorer/config/signature.hpp>
+#include <bitcoin/explorer/config/wrapper.hpp>
+#include <bitcoin/explorer/utility.hpp>
+
+/********* GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY **********/
+
+namespace libbitcoin {
+namespace explorer {
+namespace commands {
+
+/**
+ * Various localizable strings.
+ */
+#define BX_WATCH_KEY_WAITING \
+    "Watching key: %1%..."
+
+/**
+ * Class to implement the watch-key command.
+ */
+class BCX_API watch_key
+  : public command
+{
+public:
+
+    /**
+     * The symbolic (not localizable) command name, lower case.
+     */
+    static const char* symbol()
+    {
+        return "watch-key";
+    }
+
+    /**
+     * The symbolic (not localizable) former command name, lower case.
+     */
+    static const char* formerly()
+    {
+        return "monitor";
+    }
+
+    /**
+     * The member symbolic (not localizable) command name, lower case.
+     */
+    virtual const char* name()
+    {
+        return watch_key::symbol();
+    }
+
+    /**
+     * The localizable command category name, upper case.
+     */
+    virtual const char* category()
+    {
+        return "ONLINE";
+    }
+
+    /**
+     * The localizable command description.
+     */
+    virtual const char* description()
+    {
+        return "Watch the network for transactions in which a payment key participates. Requires a Libbitcoin server connection.";
+    }
+
+    /**
+     * Load program argument definitions.
+     * A value of -1 indicates that the number of instances is unlimited.
+     * @return  The loaded program argument definitions.
+     */
+    virtual system::arguments_metadata& load_arguments()
+    {
+        return get_argument_metadata()
+            .add("hash", 1);
+    }
+
+    /**
+     * Load parameter fallbacks from file or input as appropriate.
+     * @param[in]  input  The input stream for loading the parameters.
+     * @param[in]         The loaded variables.
+     */
+    virtual void load_fallbacks(std::istream& input,
+        po::variables_map& variables)
+    {
+        const auto raw = requires_raw_input();
+        load_input(get_hash_argument(), "hash", variables, input, raw);
+    }
+
+    /**
+     * Load program option definitions.
+     * BUGBUG: see boost bug/fix: svn.boost.org/trac/boost/ticket/8009
+     * @return  The loaded program option definitions.
+     */
+    virtual system::options_metadata& load_options()
+    {
+        using namespace po;
+        options_description& options = get_option_metadata();
+        options.add_options()
+        (
+            BX_HELP_VARIABLE ",h",
+            value<bool>()->zero_tokens(),
+            "Get a description and instructions for this command."
+        )
+        (
+            BX_CONFIG_VARIABLE ",c",
+            value<boost::filesystem::path>(),
+            "The path to the configuration settings file."
+        )
+        (
+            "duration,d",
+            value<uint32_t>(&option_.duration)->default_value(600),
+            "The duration of the watch in seconds, defaults to 600."
+        )
+        (
+            "hash",
+            value<system::config::hash256>(&argument_.hash),
+            "The Base16 payments search key. If not specified the address is read from STDIN."
+        );
+
+        return options;
+    }
+
+    /**
+     * Set variable defaults from configuration variable values.
+     * @param[in]  variables  The loaded variables.
+     */
+    virtual void set_defaults_from_config(po::variables_map& variables)
+    {
+    }
+
+    /**
+     * Invoke the command.
+     * @param[out]  output  The input stream for the command execution.
+     * @param[out]  error   The input stream for the command execution.
+     * @return              The appropriate console return code { -1, 0, 1 }.
+     */
+    virtual system::console_result invoke(std::ostream& output,
+        std::ostream& cerr);
+
+    /* Properties */
+
+    /**
+     * Get the value of the hash argument.
+     */
+    virtual system::config::hash256& get_hash_argument()
+    {
+        return argument_.hash;
+    }
+
+    /**
+     * Set the value of the hash argument.
+     */
+    virtual void set_hash_argument(
+        const system::config::hash256& value)
+    {
+        argument_.hash = value;
+    }
+
+    /**
+     * Get the value of the duration option.
+     */
+    virtual uint32_t& get_duration_option()
+    {
+        return option_.duration;
+    }
+
+    /**
+     * Set the value of the duration option.
+     */
+    virtual void set_duration_option(
+        const uint32_t& value)
+    {
+        option_.duration = value;
+    }
+
+private:
+
+    /**
+     * Command line argument bound variables.
+     * Uses cross-compiler safe constructor-based zeroize.
+     * Zeroize for unit test consistency with program_options initialization.
+     */
+    struct argument
+    {
+        argument()
+          : hash()
+        {
+        }
+
+        system::config::hash256 hash;
+    } argument_;
+
+    /**
+     * Command line option bound variables.
+     * Uses cross-compiler safe constructor-based zeroize.
+     * Zeroize for unit test consistency with program_options initialization.
+     */
+    struct option
+    {
+        option()
+          : duration()
+        {
+        }
+
+        uint32_t duration;
+    } option_;
+};
+
+} // namespace commands
+} // namespace explorer
+} // namespace libbitcoin
+
+#endif
