df9b6fcc6a89be8113b0296906be67506cdda7da
Phillip Mienk <mienkphi@gmail.com>
2019-09-05 23:43:09 -0700

    Fix command tests by using mainnet servers (reliable).

--- test/commands/fetch-compact-filter-headers-node.cpp
+++ test/commands/fetch-compact-filter-headers-node.cpp
@@ -35,8 +35,8 @@ BOOST_AUTO_TEST_CASE(fetch_compact_filter_headers_node__invoke__mainnet__success
     // Send p2p log output to /dev/null.
     log::initialize();
 
-    command.set_host_option(BX_TESTNET_HOST);
-    command.set_port_option(BX_TESTNET_PORT);
+    command.set_host_option(BX_MAINNET_HOST);
+    command.set_port_option(BX_MAINNET_PORT);
     command.set_hash_argument({ BLOCK_49291_HASH });
     command.set_height_argument(49250);
     command.set_type_argument(0);
