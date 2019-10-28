6a58976aad7b855e780bcc2eaaaf35de97c6732c
evoskuil <eric@voskuil.org>
2019-10-27 23:01:46 -0700

    Set secp256k1 --enable-endomorphism by default (regenerated).

--- install.sh
+++ install.sh
@@ -439,7 +439,8 @@ BOOST_OPTIONS=(
 #------------------------------------------------------------------------------
 SECP256K1_OPTIONS=(
 "--disable-tests" \
-"--enable-module-recovery")
+"--enable-module-recovery" \
+"--enable-endomorphism")
 
 # Define bitcoin-system options.
 #------------------------------------------------------------------------------
