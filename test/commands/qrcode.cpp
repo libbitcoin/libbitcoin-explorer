4a3da7228b40d22f78925f27aa33062559f7ae76
evoskuil <eric@voskuil.org>
2021-03-09 17:27:24 -0800

    Improve qrcode tests, avoid memcmp.

--- test/commands/qrcode.cpp
+++ test/commands/qrcode.cpp
@@ -24,7 +24,7 @@ BX_USING_NAMESPACES()
 BOOST_AUTO_TEST_SUITE(stub)
 BOOST_AUTO_TEST_SUITE(qrcode__invoke)
 
-#if defined WITH_QRENCODE && defined WITH_PNG
+#if defined(WITH_QRENCODE) && defined(WITH_PNG)
 BOOST_AUTO_TEST_CASE(qrcode__invoke__always__failure_error)
 {
     BX_DECLARE_COMMAND(qrcode);
@@ -37,7 +37,7 @@ BOOST_AUTO_TEST_CASE(qrcode__invoke__size_one__success)
 {
     BX_DECLARE_COMMAND(qrcode);
 
-    static const uint8_t qr_png_data[]
+    static const data_chunk expected
     {
         0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00,
         0x0d, 0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x1d, 0x00, 0x00,
@@ -64,24 +64,23 @@ BOOST_AUTO_TEST_CASE(qrcode__invoke__size_one__success)
         0xcb, 0x01, 0x45, 0x5a, 0x35, 0x29, 0x97, 0x41, 0x52, 0x77, 0x00,
         0x00, 0x00, 0x00, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82
     };
-    static const auto qr_png_data_length = sizeof(qr_png_data) / sizeof(uint8_t);
 
     command.set_module_size_option(1);
     command.set_png_option(true);
     command.set_payment_address_argument({ "12u8rC4Pxih4m59eApanRDodXcPxWiaKgg" });
     BX_REQUIRE_OKAY(command.invoke(output, error));
 
-    istream_reader in(output);
-    const auto data = in.read_bytes();
-    BOOST_REQUIRE_EQUAL(data.size(), qr_png_data_length);
-    BOOST_REQUIRE(std::memcmp(qr_png_data, data.data(), qr_png_data_length) == 0);
+    istream_reader stream(output);
+
+    // Encode as base16 so that failure message is intelligible.
+    BOOST_REQUIRE_EQUAL(encode_base16(stream.read_bytes()), encode_base16(expected));
 }
 
 BOOST_AUTO_TEST_CASE(qrcode__invoke__optional_args__success)
 {
     BX_DECLARE_COMMAND(qrcode);
 
-    const uint8_t qr_png_data[]
+    const data_chunk expected
     {
         0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00,
         0x0d, 0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x27, 0x00, 0x00,
@@ -111,7 +110,6 @@ BOOST_AUTO_TEST_CASE(qrcode__invoke__optional_args__success)
         0xf2, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42,
         0x60, 0x82
     };
-    static const auto qr_png_data_length = sizeof(qr_png_data) / sizeof(uint8_t);
 
     command.set_module_size_option(1);
     command.set_scheme_option("");
@@ -123,17 +121,17 @@ BOOST_AUTO_TEST_CASE(qrcode__invoke__optional_args__success)
     command.set_payment_address_argument({ "12u8rC4Pxih4m59eApanRDodXcPxWiaKgg" });
     BX_REQUIRE_OKAY(command.invoke(output, error));
 
-    istream_reader in(output);
-    const auto data = in.read_bytes();
-    BOOST_REQUIRE_EQUAL(data.size(), qr_png_data_length);
-    BOOST_REQUIRE(std::memcmp(qr_png_data, data.data(), qr_png_data_length) == 0);
+    istream_reader stream(output);
+
+    // Encode as base16 so that failure message is intelligible.
+    BOOST_REQUIRE_EQUAL(encode_base16(stream.read_bytes()), encode_base16(expected));
 }
 
 BOOST_AUTO_TEST_CASE(qrcode__invoke__qr_encode__success)
 {
     BX_DECLARE_COMMAND(qrcode);
 
-    const uint8_t raw_qr_data[]
+    const data_chunk expected
     {
         0x03, 0x00, 0x00, 0x00, 0x1d, 0x00, 0x00, 0x00, 0xc1, 0xc1, 0xc1,
         0xc1, 0xc1, 0xc1, 0xc1, 0xc0, 0x84, 0x03, 0x02, 0x02, 0x03, 0x03,
@@ -214,17 +212,16 @@ BOOST_AUTO_TEST_CASE(qrcode__invoke__qr_encode__success)
         0x03, 0x03, 0x02, 0x03, 0x03, 0x03, 0x03, 0x02, 0x02, 0x03, 0x02,
         0x03, 0x03
     };
-    static const auto raw_qr_data_length = sizeof(raw_qr_data) / sizeof(uint8_t);
 
     command.set_module_size_option(1);
     command.set_png_option(false);
     command.set_payment_address_argument({ "12u8rC4Pxih4m59eApanRDodXcPxWiaKgg" });
     BX_REQUIRE_OKAY(command.invoke(output, error));
 
-    istream_reader in(output);
-    const auto data = in.read_bytes();
-    BOOST_REQUIRE_EQUAL(data.size(), raw_qr_data_length);
-    BOOST_REQUIRE(std::memcmp(raw_qr_data, data.data(), raw_qr_data_length) == 0);
+    istream_reader stream(output);
+
+    // Encode as base16 so that failure message is intelligible.
+    BOOST_REQUIRE_EQUAL(encode_base16(stream.read_bytes()), encode_base16(expected));
 }
 
 #else
