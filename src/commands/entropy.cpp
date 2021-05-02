199b34ee359b25befe98317d4f180b7711e6d257
evoskuil <eric@voskuil.org>
2021-05-02 16:38:19 -0700

    Stub in new commanes: checked-encode/decode, entropy.

---
+++ src/commands/entropy.cpp
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2011-2019 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <bitcoin/explorer/commands/entropy.hpp>
+
+namespace libbitcoin {
+namespace explorer {
+namespace commands {
+
+using namespace bc::system;
+using namespace bc::system::config;
+
+console_result entropy::invoke(std::ostream& output, std::ostream& error)
+{
+    const auto bits = get_bits_option();
+
+    const auto size = ceilinged_divide(bits, byte_bits);
+
+    if (size < minimum_seed_size)
+    {
+        error << BX_ENTROPY_SHORT_BIT_LENGTH << std::endl;
+        return console_result::failure;
+    }
+
+    data_chunk entropy(size);
+    pseudo_random::fill(entropy);
+
+    output << base16(entropy) << std::endl;
+    return console_result::okay;
+}
+
+} //namespace commands
+} //namespace explorer
+} //namespace libbitcoin
---
+++ test/commands/checked-decode.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2011-2019 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "command.hpp"
+
+BX_USING_NAMESPACES()
+
+BOOST_AUTO_TEST_SUITE(offline)
+BOOST_AUTO_TEST_SUITE(checked_decode__invoke)
+
+BOOST_AUTO_TEST_CASE(checked_decode__invoke__always__failure_error)
+{
+    BX_DECLARE_COMMAND(checked_decode);
+    BX_REQUIRE_FAILURE(command.invoke(output, error));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
