9330c9a8a6fcf9e2cb84497851ca784e7bbbb898
Phillip Mienk <mienkphi@gmail.com>
2022-05-27 16:43:45 -0700

    Regenerate artifacts.

--- install.sh
+++ install.sh
@@ -415,6 +415,9 @@ build_from_tarball()
     local OPTIONS=$7
     shift 7
 
+    local SAVE_LDFLAGS="$LDFLAGS"
+    local SAVE_CPPFLAGS="$CPPFLAGS"
+
     # For some platforms we need to set ICU pkg-config path.
     if [[ ! ($BUILD) ]]; then
         if [[ $ARCHIVE == "$ICU_ARCHIVE" ]]; then
@@ -425,7 +428,6 @@ build_from_tarball()
 
     # Because ICU tools don't know how to locate internal dependencies.
     if [[ ($ARCHIVE == "$ICU_ARCHIVE") ]]; then
-        local SAVE_LDFLAGS="$LDFLAGS"
         export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
     fi
 
