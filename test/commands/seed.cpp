5281cfb370614716aa7dd1e099def193401da6f3
Kulpreet Singh <13216+kulpreet@users.noreply.github.com>
2023-08-12 00:00:31 +0200

    Obsolete bx seed command
    Issue: https://github.com/libbitcoin/libbitcoin-explorer/issues/728

--- test/commands/seed.cpp
+++ test/commands/seed.cpp
@@ -24,50 +24,13 @@ BX_USING_NAMESPACES()
 BOOST_AUTO_TEST_SUITE(offline)
 BOOST_AUTO_TEST_SUITE(seed__invoke)
 
-#define BX_SEED_BITS_TO_TERMINATED_BASE16(bits) (2 * (bits / 8) + 1)
-
-BOOST_AUTO_TEST_CASE(seed__invoke__minimum_size__okay_output)
+BOOST_AUTO_TEST_CASE(seed__invoke__always__failure_error)
 {
     BX_DECLARE_COMMAND(seed);
-
-    // Here we must explicitly set the default value, since we are bypassing
-    // the boost parser and variable bindings which implement defaults.
-    command.set_bit_length_option(128);
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BOOST_REQUIRE(output.str().size() == BX_SEED_BITS_TO_TERMINATED_BASE16(128));
-}
-
-BOOST_AUTO_TEST_CASE(seed__invoke__good_size__okay_output)
-{
-    BX_DECLARE_COMMAND(seed);
-    command.set_bit_length_option(256);
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BOOST_REQUIRE(output.str().size() == BX_SEED_BITS_TO_TERMINATED_BASE16(256));
-}
-
-BOOST_AUTO_TEST_CASE(seed__invoke_big_size_okay_output)
-{
-    BX_DECLARE_COMMAND(seed);
-    command.set_bit_length_option(512);
-    BX_REQUIRE_OKAY(command.invoke(output, error));
-    BOOST_REQUIRE(output.str().size() == BX_SEED_BITS_TO_TERMINATED_BASE16(512));
-}
-
-BOOST_AUTO_TEST_CASE(seed__invoke__too_short__failure_error)
-{
-    BX_DECLARE_COMMAND(seed);
-    command.set_bit_length_option(64);
     BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_SEED_BIT_LENGTH_UNSUPPORTED "\n");
+    BX_REQUIRE_ERROR(BX_SEED_OBSOLETE "\n");
 }
 
-BOOST_AUTO_TEST_CASE(seed__invoke__not_byte_aligned__failure_error)
-{
-    BX_DECLARE_COMMAND(seed);
-    command.set_bit_length_option(129);
-    BX_REQUIRE_FAILURE(command.invoke(output, error));
-    BX_REQUIRE_ERROR(BX_SEED_BIT_LENGTH_UNSUPPORTED "\n");
-}
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
