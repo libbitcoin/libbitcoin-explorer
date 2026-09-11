2fa80977648419cde064762bfb84552ecf509d74
Eric Voskuil <eric@voskuil.org>
2026-09-11 12:50:02 -0400

    Correct witness program type names for system v4.

--- src/config/witness.cpp
+++ src/config/witness.cpp
@@ -64,9 +64,9 @@ std::istream& operator>>(std::istream& input, witness& argument)
     input >> text;
 
     if (text == witness_p2kh_hash)
-        argument.value_ = witness::type::version_0_p2kh;
+        argument.value_ = witness::type::version0_p2kh;
     else if (text == witness_p2sh_hash)
-        argument.value_ = witness::type::version_0_p2sh;
+        argument.value_ = witness::type::version0_p2sh;
     else
         throw istream_exception(text);
 
@@ -77,9 +77,9 @@ std::ostream& operator<<(std::ostream& output, const witness& argument)
 {
     std::string text;
 
-    if (argument.value_ == witness::type::version_0_p2kh)
+    if (argument.value_ == witness::type::version0_p2kh)
         text = witness_p2kh_hash;
-    else if (argument.value_ == witness::type::version_0_p2sh)
+    else if (argument.value_ == witness::type::version0_p2sh)
         text = witness_p2sh_hash;
     else if (argument.value_ == witness::type::unknown)
         text = witness_unknown;
